afcea341c64c336b1daaa4412e2186df668af39b
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-01 14:57:12 -0500

    Time: 1 ms (96.37%), Space: 46.6 MB (5.91%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        if(arr == null) return;
+        
+        int zeroCount = 0;
+        for(int n: arr){
+            if(n == 0) zeroCount++; 
+        }
+        
+        int write = arr.length + zeroCount - 1;
+        int read = arr.length - 1; 
+        
+        while(read >= 0){
+            if(write < arr.length) arr[write] = arr[read];
+            
+            write--; 
+            
+            if(arr[read] == 0){
+                if(write < arr.length) arr[write] = 0; 
+                write--; 
+            }
+            
+            read--; 
+        }
+    }
+}
