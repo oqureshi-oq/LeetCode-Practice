188e7d9fedc44c80a9158c690ec077c571eac16f
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-03 15:12:10 -0500

    Time: 3 ms (75.47%), Space: 54.3 MB (66.33%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        if(mat == null) return null;
+        
+        int m = mat.length; 
+        int n = mat[0].length;
+        int[] ans = new int[m*n];
+        int row = 0; 
+        int col = 0;
+        boolean up = true;  
+        
+        for(int i = 0; i < m*n; i++){
+            ans[i] = mat[row][col]; 
+            
+            if(up){
+                if(row-1 >= 0 && col+1 < n){
+                    row--; 
+                    col++; 
+                } else if(col+1 < n){
+                    col++; 
+                    up = false; 
+                } else {
+                    row++; 
+                    up = false; 
+                }
+            } else {
+                if(row+1 < m && col-1 >= 0){
+                    row++;
+                    col--; 
+                } else if(row+1 < m){
+                    row++;
+                    up = true; 
+                } else {
+                    col++; 
+                    up = true; 
+                }
+            }
+        }
+        
+        return ans; 
+    }
+}
