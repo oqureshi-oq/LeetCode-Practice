a742def0e74276d0a154a507aec01637628bcab3
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-04 21:00:37 -0500

    Time: 0 ms (100.00%), Space: 39.7 MB (94.37%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        if(rowIndex < 0) return null; 
+        
+        Integer[] row = new Integer[rowIndex+1];  
+        
+        for(int i = 0; i <= rowIndex; i++){
+            int prev = 1; 
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){ 
+                    row[j] = 1; 
+                } else{
+                    int temp = row[j]; 
+                    row[j] += prev; 
+                    prev = temp; 
+                }
+            }
+        }
+        
+        return Arrays.asList(row); 
+    }
+}
