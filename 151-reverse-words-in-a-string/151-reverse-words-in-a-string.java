adf60b84ad5c19ddea633e95a030871bbd66d311
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-05 00:09:04 -0500

    Time: 5 ms (84.25%), Space: 43.4 MB (57.93%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -2,55 +2,75 @@ class Solution {
     public String reverseWords(String s) {
         if(s == null) return null; 
         
-        char[] arr = s.toCharArray();
+        char[] arr = s.toCharArray(); 
         
+        //reverse the whole string 
         reverse(arr, 0, arr.length-1); 
         
-        int start = 0; 
-        while(start < arr.length && arr[start] == ' ') 
-            start++; 
+        //get rid of leading and trailing spaces 
+        int start = getStartingIndex(arr); 
+        int end = getEndingIndex(arr); 
         
-        int end = arr.length - 1; 
-        while(end >= 0 && arr[end] == ' ')
-            end--; 
+        //reverse each word so that it reads properly
+        reverseWords(arr, start, end); 
         
-        int i = start; 
-        int j = start; 
-        while(i <= end){ 
-            while(i <= end && arr[i] != ' ')
-                i++; 
-            
-            reverse(arr, j, i-1); 
-            
-            while(i <= end && arr[i] == ' ')
-                i++; 
-            
-            j = i;
-        }
-        
-        
-        StringBuilder str = new StringBuilder(); 
-        for(int k = start; k <= end; ){
-            if(arr[k] != ' ')
-                str.append(arr[k++]);
-            
-            if(k <= end && arr[k] == ' '){
-                str.append(' ');
-                while(k <= end && arr[k] == ' ')
-                    k++; 
-            }
-        }
-     
-        return str.toString(); 
+        return cleanedString(arr, start, end); 
+    }
+    
+    public int getStartingIndex(char[] arr){
+        int i = 0; 
+        while(i < arr.length && arr[i] == ' ')
+            i++; 
+        return i; 
+    }
+    
+        public int getEndingIndex(char[] arr){
+        int i = arr.length-1; 
+        while(i >= 0 && arr[i] == ' ')
+            i--; 
+        return i; 
     }
     
     public void reverse(char[] arr, int left, int right){
         while(left < right){
-            char temp = arr[left];
+            char temp = arr[left]; 
             arr[left] = arr[right];
-            arr[right] = temp;
+            arr[right] = temp; 
             left++; 
             right--; 
         }
+    }
+    
+    public void reverseWords(char[] arr, int start, int end){
+        int left = start; 
+        int right = start; 
+        while(right <= end){
+            while(right <= end && arr[right] != ' ')
+                right++; 
+            
+            reverse(arr, left, right-1); 
+            
+            while(right <= end && arr[right] == ' ')
+                right++; 
+            
+            left = right; 
+        }
+    }
+    
+    public String cleanedString(char[] arr, int start, int end){
+        StringBuilder str = new StringBuilder(); 
+        
+        int i = start; 
+        while(i <= end){
+            if(arr[i] != ' '){
+                str.append(arr[i++]); 
+            } else {
+                str.append(' ');
+                while(i <= end && arr[i] == ' ')
+                    i++; 
+            }
+        }
+        
+        return str.toString(); 
     }
 }
