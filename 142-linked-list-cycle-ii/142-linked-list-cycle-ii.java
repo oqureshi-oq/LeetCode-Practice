4c0f7a5d13743165d34dd86c88cdbba99ed927c4
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-10 20:52:27 -0500

    Time: 0 ms (100.00%), Space: 44.7 MB (64.68%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -15,16 +15,16 @@ public class Solution {
         ListNode slow = head; 
         
         while(fast != null && fast.next != null){
-            fast = fast.next.next; 
+            fast = fast.next.next;
             slow = slow.next; 
             
             if(fast == slow){
                 fast = head; 
                 while(fast != slow){
                     fast = fast.next; 
-                    slow = slow.next;
+                    slow = slow.next; 
                 }
-                return slow; 
+                return fast; 
             }
         }
         
