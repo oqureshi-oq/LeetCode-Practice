ba028178acb5399ba738e3e5176448c691a33729
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-14 01:10:33 -0500

    Time: 17 ms (18.91%), Space: 53.7 MB (20.95%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,13 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums == null) return -1; 
+        if(nums == null) return 0; 
         
-        int x = 0; 
+        Map<Integer, Integer> map = new HashMap(); 
         
         for(int n: nums){
-            x ^= n; 
+            map.put(n, map.getOrDefault(n,0)+1); 
         }
         
-        return x; 
+        for(Map.Entry entry: map.entrySet()){
+            if((int)entry.getValue() == 1) return (int) entry.getKey(); 
+        }
+        
+        return 0; 
     }
 }
