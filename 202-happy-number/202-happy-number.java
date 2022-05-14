6541aa523f8c8e3f678501029439255f3715efb3
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-14 17:58:11 -0500

    Time: 1 ms (95.08%), Space: 41.4 MB (44.59%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isHappy(int n) {
-        Map<Integer, Integer> map = new HashMap(); 
+        int fast = n; 
+        int slow = n; 
         
         while(true){
-            int sumOfSquares = 0; 
-            int number = n; 
+            fast = getNext(getNext(fast)); 
+            slow = getNext(slow); 
             
-            if(n == 0) return false; 
-            
-            while(n != 0){
-                sumOfSquares += (n%10) * (n%10); 
-                n /= 10; 
-            }
-            
-            if(sumOfSquares == 1) return true; 
-            
-            if(map.containsKey(sumOfSquares))
-                return false; 
-            else
-                map.put(number, sumOfSquares); 
-            
-            n = sumOfSquares; 
+            if(fast == 1) return true; 
+            if(fast == slow) return false; 
         }
+    }
+    
+    public int getNext(int n){
+        int nextNumber = 0; 
+        
+        while(n != 0){
+            nextNumber += (n%10) * (n%10); 
+            n /= 10; 
+        }
+        
+        return nextNumber; 
     }
 }
