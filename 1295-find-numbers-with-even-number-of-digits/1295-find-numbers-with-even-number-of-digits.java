f6a4de88010386c87780b5730fafc097885293d2
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-17 19:04:55 -0500

    Time: 1 ms (99.21%), Space: 43.5 MB (44.71%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -14,6 +14,6 @@ class Solution {
     
     public boolean hasEvenNumDigits(int n){
         int numDigits = (int) Math.log10(Math.abs(n)) + 1; 
-        return numDigits % 2 == 0 ? true: false; 
+        return numDigits % 2 == 0;  
     }
 }
