2d94e4f4f3e65f0dc7b0e49101b8c707c9136b85
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-18 15:40:19 -0500

    Time: 1 ms (76.31%), Space: 42.7 MB (30.17%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -3,17 +3,18 @@ class Solution {
         if(nums == null) return -1;
         
         int rightSum = 0;
-        int leftSum = 0;
         
         for(int n: nums){
-            rightSum += n; 
+            rightSum += n;
         }
+        
+        int leftSum = 0;
         
         for(int i = 0; i < nums.length; i++){
             rightSum -= nums[i];
-            if(leftSum == rightSum)
-                return i;
-            leftSum += nums[i];
+            if(leftSum == rightSum) 
+                return i; 
+            leftSum += nums[i]; 
         }
         
         return -1; 
