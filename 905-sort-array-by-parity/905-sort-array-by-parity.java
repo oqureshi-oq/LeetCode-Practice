2b3456a1ef3ffa37a6c1639a1616275d8186fb3f
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-17 23:52:35 -0500

    Time: 1 ms (99.40%), Space: 48.4 MB (38.89%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -3,16 +3,16 @@ class Solution {
         if(nums == null) return null; 
         
         int left = 0; 
-        int right = nums.length - 1; 
+        int right = nums.length-1; 
         
         while(left < right){
             if(nums[left] % 2 == 0)
-                left++;
+                left++; 
             else if(nums[right] % 2 != 0)
-                right--;
-            else {
-                int temp = nums[left];
-                nums[left] = nums[right];
+                right--; 
+            else{
+                int temp = nums[left]; 
+                nums[left] = nums[right]; 
                 nums[right] = temp; 
                 left++;
                 right--; 
