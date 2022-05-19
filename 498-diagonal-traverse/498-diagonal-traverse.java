4f199a7c71eb4986b5e96596a842b842e79ae5b3
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-18 20:54:46 -0500

    Time: 2 ms (96.79%), Space: 44.1 MB (86.86%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.java
+++ 498-diagonal-traverse/498-diagonal-traverse.java
@@ -3,16 +3,16 @@ class Solution {
         if(mat == null) return null;
         
         int m = mat.length; 
-        int n = mat[0].length;
+        int n = mat[0].length; 
         int[] ans = new int[m*n];
         int row = 0; 
-        int col = 0;
-        boolean up = true;  
+        int col = 0; 
+        boolean up = true; 
         
         for(int i = 0; i < m*n; i++){
-            ans[i] = mat[row][col]; 
+            ans[i] = mat[row][col];
             
-            if(up){
+            if(up){                
                 if(row-1 >= 0 && col+1 < n){
                     row--; 
                     col++; 
@@ -25,11 +25,11 @@ class Solution {
                 }
             } else {
                 if(row+1 < m && col-1 >= 0){
-                    row++;
+                    row++; 
                     col--; 
                 } else if(row+1 < m){
                     row++;
-                    up = true; 
+                    up = true;
                 } else {
                     col++; 
                     up = true; 
