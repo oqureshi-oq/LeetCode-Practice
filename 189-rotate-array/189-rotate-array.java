920f0e9f79de1598abc194bf2ecb3bd18937c1d8
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-20 20:11:43 -0500

    Time: 1 ms (79.61%), Space: 64.2 MB (60.25%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,20 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if(nums == null || k < 0) return;
+        if(nums == null) return;
         
         k %= nums.length; 
         
         reverse(nums, 0, nums.length-1);
-        reverse(nums, 0, k-1); 
-        reverse(nums, k, nums.length-1); 
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
     }
+    
     public void reverse(int[] nums, int left, int right){
-        if(nums == null || left < 0 || right >= nums.length) return;
-        
         while(left < right){
             int temp = nums[left];
-            nums[left] = nums[right]; 
-            nums[right] = temp; 
+            nums[left] = nums[right];
+            nums[right] = temp;
             left++;
             right--; 
         }
