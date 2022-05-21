1de119d2c85395bb3494848c05c10e4807ccb428
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-21 16:24:34 -0500

    Time: 0 ms (100.00%), Space: 43.5 MB (87.34%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -15,10 +15,11 @@ public class Solution {
         ListNode slow = head; 
         
         while(fast != null && fast.next != null){
-            fast = fast.next.next; 
+            fast = fast.next.next;
             slow = slow.next; 
             
-            if(fast == slow) return true; 
+            if(fast == slow)
+                return true; 
         }
         
         return false; 
