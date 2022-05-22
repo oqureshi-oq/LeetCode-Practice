4c962871bd3a1224a64d09428ffc2be00a9fc5bb
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-22 15:52:44 -0500

    Time: 0 ms (100.00%), Space: 45.5 MB (45.89%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -18,32 +18,31 @@ class Solution {
         if(head == null) return null; 
         
         Node iter = head; 
-        
         while(iter != null){
             Node node = new Node(iter.val); 
             node.next = iter.next; 
-            iter.next = node;
+            iter.next = node; 
             iter = node.next; 
         }
         
         iter = head; 
+        while(iter != null){
+            Node addedNode = iter.next;
+            addedNode.random = null; 
+            if(iter.random != null)
+                addedNode.random = iter.random.next; 
+            iter = addedNode.next; 
+        }
+        
+        iter = head;
+        Node newHead = iter.next; 
         
         while(iter != null){
-            Node node = iter.next; 
-            if(iter.random != null)
-                node.random = iter.random.next; 
-            else
-                node.random = null; 
-            iter = node.next; 
-        }
-        
-        iter = head; 
-        Node newHead = head.next; 
-        
-        while(iter.next != null){
-            Node next = iter.next;
+            Node addedNode = iter.next;
             iter.next = iter.next.next; 
-            iter = next; 
+            if(addedNode.next != null)
+                addedNode.next = addedNode.next.next; 
+            iter = iter.next; 
         }
         
         return newHead; 
