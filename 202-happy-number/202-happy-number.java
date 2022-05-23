042ab6d74d2927ca443a34557c980be8f84e34aa
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-23 13:57:22 -0500

    Time: 2 ms (57.73%), Space: 40.5 MB (71.18%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isHappy(int n) {
-        int fast = n; 
+        int fast = n;
         int slow = n; 
         
-        while(true){
-            fast = getNext(getNext(fast)); 
+        while(true){            
+            fast = getNext(getNext(fast));
             slow = getNext(slow); 
             
-            if(fast == 1) return true; 
+            if(fast == 1) return true;
             if(fast == slow) return false; 
         }
     }
@@ -16,7 +16,7 @@ class Solution {
         int nextNumber = 0; 
         
         while(n != 0){
-            nextNumber += (n%10) * (n%10); 
+            nextNumber += (n % 10) * (n  % 10);
             n /= 10; 
         }
         
