3367fcd2bebd9c2754fe3e35f88b7178c3144d8e
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-23 10:04:20 -0500

    Time: 12 ms (27.67%), Space: 54.6 MB (7.30%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet(); 
+        if(nums == null) return 0;
         
         int actualSum = 0;
-        int sumOfDuplicates = 0; 
+        int expectedSum = 0;
+        Set<Integer> set = new HashSet(); 
         
         for(int n: nums){
             if(set.add(n))
-                sumOfDuplicates += 2*n; 
+                expectedSum += 2*n;
             actualSum += n; 
         }
         
-        return sumOfDuplicates - actualSum; 
+        return expectedSum - actualSum; 
     }
 }
