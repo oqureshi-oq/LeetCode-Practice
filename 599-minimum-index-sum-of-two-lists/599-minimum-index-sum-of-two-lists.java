2985080d1822b285c5c48b4b5e4c3a96a3e393d4
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-23 21:06:28 -0500

    Time: 18 ms (33.39%), Space: 54.1 MB (62.00%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.java
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.java
@@ -2,22 +2,30 @@ class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
         if(list1 == null || list2 == null) return null; 
         
-        Map<Integer, List<String>> map = new HashMap(); 
-        int min = list1.length + list2.length; 
+        Map<String, Integer> map1 = new HashMap(); 
         
         for(int i = 0; i < list1.length; i++){
-            for(int j = 0; j < list2.length; j++){
-                if(list1[i].equals(list2[j])){
-                    if(!map.containsKey(i+j))
-                        map.put(i+j, new ArrayList());
-                    map.get(i+j).add(list1[i]); 
-                    min = Math.min(min, i+j); 
-                }
+            map1.put(list1[i], i); 
+        }
+        
+        int min = list1.length + list2.length; 
+        Map<Integer, List<String>> map2 = new HashMap(); 
+        
+        for(int i = 0; i < list2.length; i++){
+            if(map1.containsKey(list2[i])){
+                int indexSum = map1.get(list2[i]) + i; 
+                
+                if(!map2.containsKey(indexSum))
+                    map2.put(indexSum, new ArrayList()); 
+                
+                map2.get(indexSum).add(list2[i]);
+                
+                min = Math.min(min, indexSum); 
             }
         }
         
         if(min == list1.length + list2.length) return null; 
         
-        return map.get(min).toArray(new String[map.get(min).size()]); 
+        return map2.get(min).toArray(new String[map2.get(min).size()]); 
     }
 }
