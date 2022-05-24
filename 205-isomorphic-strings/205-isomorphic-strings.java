bf545c77c07cb0162b9058acd4ae1e4e68ffeea7
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-23 19:54:36 -0500

    Time: 11 ms (50.90%), Space: 42.9 MB (58.02%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,21 +1,36 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s == null || t == null) return false; 
-        if(s.length() != t.length()) return false; 
+        if(s == null || t == null || s.length() != t.length())
+            return false; 
         
-        int[] mapStoT = new int[256]; 
-        Arrays.fill(mapStoT, -1); 
-        int[] mapTtoS = new int[256]; 
-        Arrays.fill(mapTtoS, -1); 
+        return isEqual(transform(s), transform(t));
+    }
+    
+    public int[] transform(String s){
+        if(s == null) return null; 
+        
+        int[] map = new int[256]; 
+        int[] arr = new int[s.length()]; 
+        Arrays.fill(map, -1); 
         
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            char d = t.charAt(i); 
             
-            if(mapStoT[c] == -1 && mapTtoS[d] == -1){
-                mapStoT[c] = d;
-                mapTtoS[d] = c; 
-            } else if(mapStoT[c] != d || mapTtoS[d] != c)
+            if(map[c] == -1)
+                map[c] = i;
+            
+            arr[i] = map[c]; 
+        }
+        
+        return arr; 
+    }
+    
+    public boolean isEqual(int[] s, int[] t){
+        if(s == null || t == null || s.length != t.length)
+            return false; 
+        
+        for(int i = 0; i < s.length; i++){
+            if(s[i] != t[i])
                 return false; 
         }
         
