cc68bcaf5853490da3bfb3466bfe3ace20446dc2
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-25 20:48:32 -0500

    Time: 6 ms (96.62%), Space: 46.1 MB (79.27%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs == null || strs.length == 0) return null; 
+        if(strs == null || strs.length == 0) return null;
         
         Map<String, List<String>> map = new HashMap(); 
         
         for(String s: strs){
             char[] arr = s.toCharArray(); 
-            Arrays.sort(arr); 
-            String sortedString = String.valueOf(arr); 
-            if(!map.containsKey(sortedString))
-                map.put(sortedString, new ArrayList()); 
-            map.get(sortedString).add(s); 
+            char[] charMap = new char[26]; 
+            for(char c: arr){
+                charMap[c - 'a']++; 
+            }
+            String key = String.valueOf(charMap); 
+            if(!map.containsKey(key))
+                map.put(key, new ArrayList());
+            map.get(key).add(s);
         }
         
-        List<List<String>> list = new ArrayList(); 
-        
-        for(String s: map.keySet()){
-            //System.out.println(map.get(s)); 
-            list.add(map.get(s)); 
-        }
-        
-        return list; 
+        return new ArrayList(map.values()); 
     }
 }
