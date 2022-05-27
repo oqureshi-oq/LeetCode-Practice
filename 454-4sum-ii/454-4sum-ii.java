aa0f9675636681a314598f3d3d74a9b14fbbe40b
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-27 00:53:08 -0500

    Time: 244 ms (18.03%), Space: 58.2 MB (32.99%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        Map<Integer, Integer> map12 = new HashMap(); 
-        Map<Integer, Integer> map34 = new HashMap(); 
+        Map<Integer, Integer> map = new HashMap();  
         
         for(int i = 0; i < nums1.length; i++){
             for(int j = 0; j < nums2.length; j++){
-                int sum12 = nums1[i] + nums2[j]; 
-                int sum34 = nums3[i] + nums4[j]; 
+                int sum = nums1[i] + nums2[j];  
                 
-                map12.put(sum12, map12.getOrDefault(sum12, 0) + 1); 
-                map34.put(sum34, map34.getOrDefault(sum34, 0) + 1); 
+                map.put(sum, map.getOrDefault(sum, 0) + 1); 
             }
         }
         
-        int count = 0; 
+        int count = 0;
         
-        for(int ij: map12.keySet()){
-            if(map34.containsKey(0-ij))
-                count += map12.get(ij) * map34.get(0-ij); 
+        for(int i: nums3){
+            for(int j: nums4){
+                count += map.getOrDefault(-(i+j), 0); 
+            }
         }
         
         return count; 
