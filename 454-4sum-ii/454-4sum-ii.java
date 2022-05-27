6628e75d4da858139aca29501cb8d5a445c3e632
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-27 00:36:54 -0500

    Time: 215 ms (30.20%), Space: 70.4 MB (5.14%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,26 +1,23 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        if(nums1 == null || nums2 == null || nums3 == null || nums4 == null)
-            return -1;
-        
-        if(nums1.length != nums2.length || nums2.length != nums3.length || 
-           nums3.length != nums4.length)
-            return -1; 
-        
-        Map<Integer, Integer> map1 = new HashMap(); 
-        Map<Integer, Integer> map2 = new HashMap(); 
-        int count = 0; 
+        Map<Integer, Integer> map12 = new HashMap(); 
+        Map<Integer, Integer> map34 = new HashMap(); 
         
         for(int i = 0; i < nums1.length; i++){
             for(int j = 0; j < nums2.length; j++){
-                map1.put(nums1[i] + nums2[j], map1.getOrDefault(nums1[i] + nums2[j], 0) + 1); 
-                map2.put(nums3[i] + nums4[j], map2.getOrDefault(nums3[i] + nums4[j], 0) + 1); 
+                int sum12 = nums1[i] + nums2[j]; 
+                int sum34 = nums3[i] + nums4[j]; 
+                
+                map12.put(sum12, map12.getOrDefault(sum12, 0) + 1); 
+                map34.put(sum34, map34.getOrDefault(sum34, 0) + 1); 
             }
         }
         
-        for(int key: map1.keySet()){
-            if(map2.containsKey(0 - key))
-                count += map1.get(key) * map2.get(0 - key); 
+        int count = 0; 
+        
+        for(int ij: map12.keySet()){
+            if(map34.containsKey(0-ij))
+                count += map12.get(ij) * map34.get(0-ij); 
         }
         
         return count; 
