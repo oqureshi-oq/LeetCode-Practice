9caf99ab261fac23ef33200a80e2ded21cf4ea4b
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-27 19:01:17 -0500

    Time: 8 ms (98.29%), Space: 45.1 MB (81.54%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,31 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if(nums == null) return null;
+        if(nums == null) return null; 
         
-        Map<Integer, Integer> map = new HashMap(); 
+        Map<Integer, Integer> count = new HashMap(); 
         
         for(int n: nums){
-            map.put(n, map.getOrDefault(n, 0) + 1); 
+            count.put(n, count.getOrDefault(n,0) + 1);; 
         }
         
-        ArrayList<Integer>[] frequency = new ArrayList[nums.length+1]; 
+        List<Integer>[] freqBucket = new List[nums.length+1]; 
         
-        for(int n: map.keySet()){
-            int index = map.get(n); 
-            
-            if(frequency[index] == null)
-                frequency[index] = new ArrayList(); 
-            
-            frequency[index].add(n); 
+        for(int n: count.keySet()){
+            if(freqBucket[count.get(n)] == null)
+                freqBucket[count.get(n)] = new ArrayList(); 
+            freqBucket[count.get(n)].add(n); 
         }
         
-        int[] ans = new int[k];
-        
-        for(int i = frequency.length-1, j = 0; j < k && i >= 0; i--){
-            if(frequency[i] == null) continue; 
-            
-            for(int l = 0; j < k && l < frequency[i].size(); l++)
-                ans[j++] = frequency[i].get(l); 
+        int[] ans = new int[k]; 
+        int i = 0;
+        for(int j = freqBucket.length-1; j >=0 && i < k; j--){
+            if(freqBucket[j] != null){
+                for(int l = 0; i < k && l < freqBucket[j].size(); l++){
+                    ans[i++] = freqBucket[j].get(l); 
+                }
+            }
         }
         
         return ans; 
