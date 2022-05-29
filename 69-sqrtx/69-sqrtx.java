b97469d0bcff3f61113384a0e924d46b3268a7b1
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-29 01:01:06 -0500

    Time: 1 ms (99.70%), Space: 39.3 MB (93.81%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -7,22 +7,15 @@ class Solution {
         while(left <= right){
             int mid = left + (right-left)/2; 
             
-            if(mid > x/mid){
-                right = mid-1;
-            } else {
-                if(mid+1 > x/(mid+1)) return mid;
-                left = mid+1; 
+            if(mid <= x/mid){
+                if(mid+1 > x/(mid+1)) {
+                    return mid; 
+                } else {
+                    left = mid+1;
+                }
+            } else{
+                right = mid-1; 
             }
-            
-            // if(mid < x/mid){
-            //     if(mid+1 > x/(mid+1)) {
-            //         return mid; 
-            //     } else {
-            //         left = mid+1;
-            //     }
-            // } else{
-            //     right = mid-1; 
-            // }
         }
         
         System.out.println(left);
