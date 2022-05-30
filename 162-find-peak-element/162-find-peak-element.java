2162e9b2bd43e0a78974a4f2bdc2ec80a632f604
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-30 15:13:16 -0500

    Time: 0 ms (100.00%), Space: 42.9 MB (41.12%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -9,7 +9,7 @@ class Solution {
         while(left < right){
             int mid = left + (right - left)/2; 
             
-            if(nums[mid] < nums[mid+1])
+            if(mid+1 < nums.length && nums[mid] < nums[mid+1])
                 left = mid+1; 
             else
                 right = mid; 
