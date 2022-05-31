619ff55480af98b64459c8356db916b5df1246cf
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-30 23:26:00 -0500

    Time: 8 ms (7.12%), Space: 46.8 MB (65.32%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return new int[]{-1, -1};
+        if(nums == null || nums.length == 0) return new int[]{-1, -1}; 
         
-        int[] ans = new int[2]; 
+        int[] ans = new int[2];
         ans[0] = findFirst(nums, target); 
         ans[1] = findLast(nums, target); 
         
@@ -10,54 +10,44 @@ class Solution {
     }
     
     public int findFirst(int[] nums, int target){
-        if(nums == null) return -1; 
+        if(nums == null || nums.length == 0) return -1; 
         
         int left = 0;
         int right = nums.length - 1; 
         
-        while(right - left > 1){
+        while(left < right){
             int mid = left + (right - left)/2; 
             
             if(nums[mid] == target)
                 right = mid; 
-            else if(target < nums[mid])
-                right = mid-1;
-            else
+            else if(nums[mid] < target)
                 left = mid+1; 
+            else
+                right = mid-1; 
         }
-        
-        if(nums[left] == target)
-            return left;
-        
-        if(nums[right] == target)
-            return right; 
-        
-        return -1; 
+
+        return left >= 0 && left < nums.length && nums[left] == target ? left: -1; 
     }
     
     public int findLast(int[] nums, int target){
-        if(nums == null) return -1; 
+        if(nums == null || nums.length == 0) return -1; 
         
-        int left = 0;
+        int left = 0; 
         int right = nums.length - 1; 
         
-        while(right - left > 1){
-            int mid = left + (right - left)/2; 
+        while(left < right){
+            int mid = left + (right - left)/2+1; 
+            
+            System.out.println("mid: " + mid); 
             
             if(nums[mid] == target){
                 left = mid;
             }else if(nums[mid] < target)
-                left = mid+1;
+                left = mid+1; 
             else
                 right = mid-1; 
         }
-    
-        if(nums[right] == target)
-            return right; 
         
-        if(nums[left] == target)
-            return left;
-        
-        return -1; 
+        return left >= 0 && left < nums.length && nums[left] == target ? left: -1; 
     }
 }
