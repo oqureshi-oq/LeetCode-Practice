fae157bb286fece5dd24d1ec6b3447c242484919
Omar Qureshi <oqureshi.oq@gmail.com>
2022-05-30 23:12:53 -0500

    Time: 1 ms, Memory: 47.4 MB - LeetHub

---
+++ search-for-a-range/search-for-a-range.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return new int[]{-1, -1}; 
+        
+        int[] ans = new int[2];
+        ans[0] = findFirst(nums, target); 
+        ans[1] = findLast(nums, target); 
+        
+        return ans; 
+    }
+    
+    public int findFirst(int[] nums, int target){
+        if(nums == null || nums.length == 0) return -1; 
+        
+        int left = 0;
+        int right = nums.length - 1; 
+        
+        while(left < right){
+            int mid = left + (right - left)/2; 
+            
+            if(nums[mid] == target)
+                right = mid; 
+            else if(nums[mid] < target)
+                left = mid+1; 
+            else
+                right = mid-1; 
+        }
+        
+        return nums[left] == target ? left: -1; 
+    }
+    
+    public int findLast(int[] nums, int target){
+        if(nums == null || nums.length == 0) return -1; 
+        
+        int left = 0; 
+        int right = nums.length - 1; 
+        
+        while(left < right){
+            int mid = left + (right - left)/2+1; 
+            
+            if(nums[mid] == target){
+                left = mid;
+            }else if(nums[mid] < target)
+                left = mid+1; 
+            else
+                right = mid-1; 
+        }
+        
+        return nums[right] == target ? right: -1; 
+    }
+}
