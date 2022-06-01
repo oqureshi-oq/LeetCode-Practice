a5b48ccd0ca8967aba066465c93785631a82ec18
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-01 01:25:07 -0500

    Time: 38 ms (86.84%), Space: 76.8 MB (60.45%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.java
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -1,36 +1,55 @@
 class SnapshotArray {
     
-    private TreeMap<Integer, Integer>[] arr; //key is snapID
-    int snapid; 
+    private Map<Integer, Integer>[] arr; 
+    private int snap_id; 
 
     public SnapshotArray(int length) {
-        arr = new TreeMap[length]; 
-        snapid = 0;
+        arr = new Map[length]; 
+        snap_id = 0; 
     }
     
-    public void set(int index, int val) {   
+    public void set(int index, int val) {
+        if(index < 0 || index >= arr.length) return; 
+        
         if(arr[index] == null){
-            arr[index] = new TreeMap();
+            arr[index] = new HashMap();
             arr[index].put(0, 0); 
         }
         
-        arr[index].put(snapid, val); 
+        arr[index].put(snap_id, val); 
     }
     
     public int snap() {
-        snapid++; 
-        return snapid-1; 
+        return snap_id++; 
     }
     
     public int get(int index, int snap_id) {
-        if(arr[index] == null){
-            arr[index] = new TreeMap();
-            arr[index].put(0, 0); 
-        } 
+        if(index < 0 || index >= arr.length) return -1; 
         
-        return arr[index].floorEntry(snap_id).getValue();
+        if(arr[index] == null){
+            arr[index] = new HashMap(); 
+            arr[index].put(0, 0); 
+        }
+        
+        if(arr[index].containsKey(snap_id))
+            return arr[index].get(snap_id); 
+        
+        int left = 0;
+        int right = snap_id; 
+        int mid = left + (right-left)/2;
+        
+        for(int i = snap_id; i >= 0; i--){
+            if(arr[index].containsKey(i))
+                return arr[index].get(i); 
+        }
+        
+        return arr[index].get(0); 
     }
 }
+
+//[0, 1, 3, 6, 8]
+
+//<=5
 
 /**
  * Your SnapshotArray object will be instantiated and called as such:
