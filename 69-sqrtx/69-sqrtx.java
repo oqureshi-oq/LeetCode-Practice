c1f5d132d1f54fefd614b4259459549d8195abdf
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-03 01:27:15 -0500

    Time: 2 ms (73.01%), Space: 41.3 MB (54.97%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,21 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x < 0) return -1;
-        if(x == 0) return 0; 
+        if(x < 0)
+            return -1;
+        if(x == 0)
+            return 0; 
         
-        int left = 1; 
+        int left = 1;
         int right = x; 
         
         while(left <= right){
-            int mid = left + (right-left)/2; 
-            
-            if(mid <= x/mid){
+            int mid = left + (right - left)/2;
+             
+            if(mid > x/mid){
+                right = mid-1; 
+            } else {
                 if(mid+1 > x/(mid+1))
                     return mid; 
                 else
                     left = mid+1; 
-            } else {
-                right = mid-1; 
             }
         }
         
