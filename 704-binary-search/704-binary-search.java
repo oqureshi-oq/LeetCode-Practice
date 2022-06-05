1fe1169750872995be9062fd88e53615815e13d4
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-04 22:41:15 -0500

    Time: 0 ms (100.00%), Space: 54.8 MB (19.50%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -3,17 +3,17 @@ class Solution {
         if(nums == null) return -1;
         
         int left = 0;
-        int right = nums.length - 1; 
+        int right = nums.length - 1;
         
         while(left <= right){
-            int mid = left + (right-left)/2; 
+            int mid = left + ((right - left) >> 1); 
             
             if(nums[mid] == target)
-                return mid; 
-            else if(target < nums[mid])
-                right = mid-1; 
-            else
+                return mid;
+            else if(nums[mid] < target)
                 left = mid+1; 
+            else
+                right = mid-1; 
         }
         
         return -1; 
