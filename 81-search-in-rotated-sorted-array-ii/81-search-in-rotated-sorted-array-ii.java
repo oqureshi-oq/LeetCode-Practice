b90691e36079f568d5eb96df7c494532f8e3fd32
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-08 11:14:25 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (85.47%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -3,13 +3,13 @@ class Solution {
         if(nums == null) return false;
         
         int left = 0;
-        int right = nums.length -1; 
+        int right = nums.length - 1;
         
         while(left <= right){
-            int mid = left + ((right - left) >> 1); 
+            int mid = left + ((right - left) >> 1);
             
             if(nums[mid] == target){
-                return true; 
+                return true;
             } else if(nums[left] == nums[mid] && nums[mid] == nums[right]){
                 left++;
                 right--;
@@ -17,7 +17,7 @@ class Solution {
                 if(nums[left] <= target && target < nums[mid])
                     right = mid-1;
                 else
-                    left = mid+1; 
+                    left = mid+1;
             } else {
                 if(nums[mid] < target && target <= nums[right])
                     left = mid+1;
@@ -26,6 +26,6 @@ class Solution {
             }
         }
         
-        return false; 
+        return false;
     }
 }
