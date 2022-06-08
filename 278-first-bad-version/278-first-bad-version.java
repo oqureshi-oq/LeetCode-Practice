c62f08838b4e8f6f5d01f27d356b44de4e5d9297
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-08 11:29:42 -0500

    Time: 16 ms (62.93%), Space: 41.5 MB (7.22%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,7 +3,7 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        if(n < 1)
+        if(n <= 0)
             return -1;
         
         int left = 1;
@@ -13,11 +13,11 @@ public class Solution extends VersionControl {
             int mid = left + ((right - left) >> 1);
             
             if(isBadVersion(mid))
-                right = mid;
+                right = mid; 
             else
                 left = mid+1; 
         }
         
-        return isBadVersion(left) ? left: -1; 
+        return isBadVersion(left) ? left: -1;
     }
 }
