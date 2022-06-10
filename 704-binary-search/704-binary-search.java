182ca4b9d203d70fffe1fa9b9e5191546d15c879
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-10 14:32:35 -0500

    Time: 0 ms (100.00%), Space: 43.1 MB (78.82%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,7 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null)
-            return -1;
+        if(nums == null) return -1;
         
         int left = 0;
         int right = nums.length - 1;
