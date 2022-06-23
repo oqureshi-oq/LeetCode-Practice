d7ea00cd54180db85e1f2abbae7a8cf8e5f496b4
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-22 21:29:36 -0500

    Time: 1 ms (99.19%), Space: 42.6 MB (84.53%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,61 +1,38 @@
 class Solution {
-    public void duplicateZeros(int[] arr){
-        if(arr == null) return;
+    public void duplicateZeros(int[] arr) {
+        if(arr == null) return; 
         
-        int read = 0;
-        int count = 0;
-        while(count < arr.length){
-            if(arr[read] == 0)
-                count++;
-            count++; 
-            read++; 
-        }
+        int zeroCount = getZeroCount(arr);
+        int write = arr.length + zeroCount - 1;
+        int read = arr.length - 1;
         
-        read--; 
-        int write = arr.length - 1;
-        if(count > arr.length && arr[read] == 0)
-            arr[write--] = arr[read--]; 
         
         while(read >= 0){
-            arr[write--] = arr[read];
-            if(arr[read] == 0)
-                arr[write--] = arr[read];
+            if(write < arr.length)
+                arr[write] = arr[read];
+            
+            write--; 
+            
+            if(arr[read] == 0){
+                if(write < arr.length)
+                    arr[write] = 0;
+                write--;
+            }
+            
             read--; 
         }
     }
-//     public void duplicateZeros(int[] arr) {
-//         if(arr == null) return;
-        
-//         int zeroCount = getZeroCount(arr);
-//         int write = arr.length + zeroCount - 1;
-//         int read = arr.length - 1;
-        
-//         while(read >= 0){
-//             if(write < arr.length)
-//                 arr[write] = arr[read];
-            
-//             write--; 
-            
-//             if(arr[read] == 0){
-//                 if(write < arr.length)
-//                     arr[write] = 0;
-//                 write--; 
-//             }
-            
-//             read--; 
-//         }
-//     }
     
-//     public int getZeroCount(int[] arr){
-//         if(arr == null) return -1;
+    public int getZeroCount(int[] arr){
+        if(arr == null) return -1;
         
-//         int zeroCount = 0;
+        int zeroCount = 0;
         
-//         for(int n: arr){
-//             if(n == 0)
-//                 zeroCount++;
-//         }
+        for(int n: arr){
+            if(n == 0)
+                zeroCount++;
+        }
         
-//         return zeroCount; 
-//     }
+        return zeroCount; 
+    }
 }
