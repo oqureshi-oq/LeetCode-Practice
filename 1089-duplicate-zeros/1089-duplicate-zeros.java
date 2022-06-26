85970fbbc4cb8af18df0804f07e1ba342e6f018d
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-26 11:02:54 -0500

    Time: 20 ms (17.38%), Space: 46.6 MB (7.52%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,63 +1,15 @@
 class Solution {
-    public void duplicateZeros(int[] arr){
+    public void duplicateZeros(int[] arr) {
         if(arr == null) return;
         
-        int count = 0;
-        int read = 0;
-        while(count < arr.length){
-            count++;
-            if(arr[read] == 0)
-                count++; 
-            read++; 
-        }
-        
-        read--; 
-        int write = arr.length - 1;
-        
-        if(count > arr.length)
-            arr[write--] = arr[read--];
-        
-        while(read >= 0){
-            arr[write--] = arr[read];
-            if(arr[read] == 0)
-                arr[write--] = 0;
-            read--; 
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0) continue;
+            
+            for(int j = arr.length - 2; i <= j; j--){
+                arr[j+1] = arr[j];
+            }
+            
+            i++; 
         }
     }
-//     public void duplicateZeros(int[] arr) {
-//         if(arr == null) return; 
-        
-//         int zeroCount = getZeroCount(arr);
-//         int write = arr.length + zeroCount - 1;
-//         int read = arr.length - 1;
-        
-        
-//         while(read >= 0){
-//             if(write < arr.length)
-//                 arr[write] = arr[read];
-            
-//             write--; 
-            
-//             if(arr[read] == 0){
-//                 if(write < arr.length)
-//                     arr[write] = 0;
-//                 write--;
-//             }
-            
-//             read--; 
-//         }
-//     }
-    
-//     public int getZeroCount(int[] arr){
-//         if(arr == null) return -1;
-        
-//         int zeroCount = 0;
-        
-//         for(int n: arr){
-//             if(n == 0)
-//                 zeroCount++;
-//         }
-        
-//         return zeroCount; 
-//     }
 }
