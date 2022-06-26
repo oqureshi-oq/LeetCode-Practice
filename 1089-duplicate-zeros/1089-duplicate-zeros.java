2af770f332d565951e1da6d10a97e1fcb549a346
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-26 11:20:38 -0500

    Time: 1 ms (99.26%), Space: 42.2 MB (97.87%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -2,6 +2,42 @@ class Solution {
     public void duplicateZeros(int[] arr) {
         if(arr == null) return;
         
+        int zeroCount = getZeroCount(arr);
+        int read = arr.length - 1;
+        int write = arr.length + zeroCount - 1;
+        
+        while(read >= 0){
+            if(write < arr.length)
+                arr[write] = arr[read];
+            
+            write--;
+            
+            if(arr[read] == 0){
+                if(write < arr.length)
+                    arr[write] = 0;
+                write--;
+            }
+            
+            read--; 
+        }
+    }
+    
+    public int getZeroCount(int[] arr){
+        if(arr == null) return -1;
+        
+        int zeroCount = 0;
+        
+        for(int n: arr){
+            if(n == 0)
+                zeroCount++;
+        }
+        
+        return zeroCount; 
+    }
+    
+    public void duplicateZeros3(int[] arr) {
+        if(arr == null) return;
+        
         int count = 0;
         int read = 0;
         int write = arr.length - 1; 
