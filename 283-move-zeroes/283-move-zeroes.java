7b73841b875455a123ec3379521790563b65afa9
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-26 23:30:22 -0500

    Time: 2 ms (78.18%), Space: 54.9 MB (42.52%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -2,11 +2,11 @@ class Solution {
     public void moveZeroes(int[] nums) {
         if(nums == null) return;
         
-        int write = 0; 
+        int write = 0;
         
         for(int n: nums){
             if(n != 0)
-                nums[write++] = n; 
+                nums[write++] = n;
         }
         
         while(write < nums.length)
