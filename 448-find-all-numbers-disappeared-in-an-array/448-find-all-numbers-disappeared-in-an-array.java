26f90d2caafe94d90434bd0d09811271da1b923a
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-27 23:57:06 -0500

    Time: 10 ms (46.66%), Space: 65.7 MB (67.55%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -12,6 +12,8 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > 0)
                 list.add(i+1); 
+            else
+                nums[i] *= -1; 
         }
         
         return list; 
