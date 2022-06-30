daa4f38d39910847238483cd1fd8f0debbc686f4
Omar Qureshi <oqureshi.oq@gmail.com>
2022-06-29 20:14:36 -0500

    Time: 1 ms (63.60%), Space: 42.1 MB (45.88%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,17 +1,14 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack == null || needle == null) return -1; 
+        if(haystack == null || needle == null) return -1;
+        
         if(needle.length() == 0) return 0; 
         
         for(int i = 0; i <= haystack.length() - needle.length(); i++){
-            int j = i; 
-            int k = 0; 
-            while(k < needle.length() && haystack.charAt(j) == needle.charAt(k)){
-                j++; 
-                k++; 
-            }
-            
-            if(k == needle.length()) 
+            int j = 0;
+            while(j < needle.length() && haystack.charAt(i+j) == needle.charAt(j))
+                j++;
+            if(j == needle.length())
                 return i; 
         }
         
