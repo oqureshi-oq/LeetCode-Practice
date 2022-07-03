dedf4a31902dc3067e0e79fa9cb718c135b02874
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-03 11:56:38 -0500

    Time: 2 ms (77.09%), Space: 52.3 MB (54.86%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -3,15 +3,15 @@ class Solution {
         if(nums == null) return -1;
         
         int rightSum = 0;
+        int leftSum = 0; 
+        
         for(int n: nums){
             rightSum += n; 
         }
         
-        int leftSum = 0; 
-        
         for(int i = 0; i < nums.length; i++){
             rightSum -= nums[i];
-            if(leftSum == rightSum) 
+            if(leftSum == rightSum)
                 return i; 
             leftSum += nums[i];
         }
