0a67328aff00e5af06413013c207a39d0cf89e2e
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-02 20:06:39 -0500

    Time: 2 ms (73.96%), Space: 53.6 MB (76.29%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -3,16 +3,15 @@ class Solution {
         if(arr == null || arr.length < 3) return false; 
         
         int i = 0; 
-        
         while(i+1 < arr.length && arr[i] < arr[i+1])
-            i++;
+            i++; 
         
-        if(i == 0 || i == arr.length-1)
+        if(i == 0 || i == arr.length - 1)
             return false;
         
         while(i+1 < arr.length && arr[i] > arr[i+1])
             i++; 
         
-        return i == arr.length - 1; 
+        return i == arr.length-1; 
     }
 }
