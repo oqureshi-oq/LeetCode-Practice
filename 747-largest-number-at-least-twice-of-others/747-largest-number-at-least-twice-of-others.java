37f935da0ba35263b6c23155990d2b70e96e661b
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-03 13:36:34 -0500

    Time: 1 ms (82.15%), Space: 42.6 MB (11.67%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
@@ -1,24 +1,21 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        if(nums == null) return -1; 
+        if(nums == null || nums.length == 0) return -1;
         
         Integer first = null; 
-        Integer second = null;
+        Integer second = null; 
         int index = -1; 
         
         for(int i = 0; i < nums.length; i++){
             if(first == null || nums[i] > first){
                 second = first;
-                first = nums[i];
+                first = nums[i]; 
                 index = i; 
             } else if(second == null || nums[i] > second){
-                second = nums[i]; 
+                second = nums[i];
             }
         }
         
-        if(first == null) return -1; 
-        if(second == null) return index; 
-        
-        return first >= 2*second ? index: -1; 
+        return second == null || first >= 2 * second ? index: -1; 
     }
 }
