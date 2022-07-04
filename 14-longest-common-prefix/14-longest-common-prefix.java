c98e6984942bda5b08f6bb2afca5f7764cbc926c
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-04 02:41:14 -0500

    Time: 1 ms (87.68%), Space: 42.4 MB (39.44%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,12 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs == null) return null; 
+        if(strs == null || strs.length == 0) return ""; 
         
         for(int i = 0; i < strs[0].length(); i++){
-            char c = strs[0].charAt(i); 
+            char c = strs[0].charAt(i);
             for(int j = 1; j < strs.length; j++){
                 if(i >= strs[j].length() || c != strs[j].charAt(i))
-                    return strs[0].substring(0, i); 
+                    return strs[0].substring(0,i); 
             }
         }
         
