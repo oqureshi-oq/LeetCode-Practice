b0db652a996845d926d2319b5764a56251b5707c
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-11 20:02:08 -0500

    Time: 1 ms (74.22%), Space: 42.8 MB (38.13%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null) return -1;
+        if(nums == null) return -1; 
         
         int left = 0;
         int right = nums.length - 1; 
         
         while(left <= right){
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left)/2; 
             
             if(nums[mid] == target){
                 return mid; 
@@ -23,6 +23,6 @@ class Solution {
             }
         }
         
-        return -1;
+        return -1; 
     }
 }
