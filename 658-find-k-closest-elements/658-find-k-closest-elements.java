303582475f6e71d324dbc1109413f674cd6ab9e9
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-14 23:45:50 -0500

    Time: 4 ms (94.18%), Space: 44.4 MB (89.59%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,25 +1,30 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        if(arr == null || arr.length == 0 || k <= 0 || k > arr.length) 
-            return new ArrayList();
+        if(arr == null || k > arr.length || k <= 0) 
+            return new ArrayList(); 
         
-        int closestIndex = getClosestIndex(arr, x);
-        int left = closestIndex;
-        int right = closestIndex;
+        int xIndex = binarySearch(arr, x); 
         
-        while(right - left + 1 < k){
-            if(left-1 >= 0 && right+1 < arr.length){
+        if(xIndex == -1)
+            return new ArrayList(); 
+        
+        int left = xIndex;
+        int right = xIndex;
+        
+        while(right - left + 1 != k){
+            if(right + 1 < arr.length && left - 1 >= 0){
                 if(Math.abs(arr[left-1] - x) <= Math.abs(arr[right+1] - x))
                     left--;
                 else
                     right++; 
-            } else if(left-1 >= 0)
-                left--;
+            } else if(right+1 < arr.length)
+                right++;
             else
-                right++; 
+                left--; 
         }
         
         List<Integer> list = new ArrayList(); 
+        
         for(int i = left; i <= right; i++){
             list.add(arr[i]);
         }
@@ -27,30 +32,31 @@ class Solution {
         return list; 
     }
     
-    public int getClosestIndex(int[] arr, int x){
-        if(arr == null || arr.length == 0) return -1;
+    public int binarySearch(int[] arr, int x){
+        if(arr == null) return -1;
         
         int left = 0;
-        int right = arr.length - 1;
-        int closestIndex = 0;
+        int right = arr.length-1;
         
         while(left <= right){
-            int mid = left + ((right - left) >> 1);
-            
-            int currDiff = Math.abs(x - arr[closestIndex]);
-            int midDiff = Math.abs(x - arr[mid]); 
-            
-            if(midDiff < currDiff || (midDiff == currDiff && mid < closestIndex))
-                closestIndex = mid;
+            int mid = left + (right - left)/2;
             
             if(arr[mid] == x)
-                break;
+                return mid; 
             else if(arr[mid] < x)
                 left = mid+1;
             else
-                right = mid-1;
+                right = mid-1; 
         }
         
-        return closestIndex; 
+        if(left < arr.length && right >= 0){
+            if(Math.abs(arr[right] - x) <= Math.abs(arr[left] - x))
+                return right;
+            else
+                return left; 
+        }
+        
+        if(left >= arr.length) return right;
+        return left; 
     }
 }
