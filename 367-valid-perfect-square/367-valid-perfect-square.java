0816050af9554d1af6a37d7487f435ba5d834234
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-16 22:58:01 -0500

    Time: 0 ms (100.00%), Space: 41.1 MB (27.22%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num < 0) return false;
-        if(num == 0) return true;
+        if(num <= 0) return false; 
         
         int left = 1;
         int right = num; 
         
         while(left <= right){
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left)/2;
             
-            if(mid > num/mid){
+            if(num % mid == 0 && mid == num/mid)
+                return true;
+            else if(num/mid < mid)
                 right = mid-1;
-            } else {
-                if(num % mid == 0 && mid == num/mid) return true;
-                left = mid+1; 
-            }
+            else
+                left = mid+1;
         }
         
         return false; 
