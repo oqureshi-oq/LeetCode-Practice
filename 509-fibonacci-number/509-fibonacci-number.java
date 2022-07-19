07dc74c3f4da12a07ee82974898885e8f8018298
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-18 23:53:26 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (87.09%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,28 @@
 class Solution {
+    
+    Map<Integer, Integer> map; 
+    
     public int fib(int n) {
+        map = new HashMap(); 
+        map.put(0, 0);
+        map.put(1, 1); 
+        return fibHelper(n); 
+    }
+    
+    public int fibHelper(int n){
         if(n == 0) return 0;
         if(n == 1) return 1; 
         
-        return fib(n-1) + fib(n-2); 
+        int ans = 0; 
+        
+        if(!map.containsKey(n-1))
+            map.put(n-1, fibHelper(n-1)); 
+        
+        if(!map.containsKey(n-2))
+            map.put(n-2, fibHelper(n-2));
+        
+        map.put(n, map.get(n-1) + map.get(n-2));
+        
+        return map.get(n); 
     }
 }
