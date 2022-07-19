fa743ad7a9d2a2d1acada6c0088d990e3fe2afc6
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-18 21:33:17 -0500

    Time: 1 ms (99.91%), Space: 55.4 MB (39.90%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -8,7 +8,7 @@ class Solution {
         while(left < right){
             char temp = s[left];
             s[left] = s[right];
-            s[right] = temp; 
+            s[right] = temp;
             left++;
             right--; 
         }
