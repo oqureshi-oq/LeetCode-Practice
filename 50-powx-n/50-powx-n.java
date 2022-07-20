4166331109e60000533096dc9dde1cb399e69892
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-20 12:44:15 -0500

    Time: 1 ms (88.22%), Space: 42.2 MB (73.88%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,11 +1,10 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n == 0) return 1; 
+        if(n == 0) return 1;
         if(n == 1) return x; 
         double half = myPow(x, Math.abs(n/2)); 
         half *= half; 
-        if(Math.abs(n) % 2 == 1)
-            half *= x; 
+        if(Math.abs(n) % 2 == 1) half *= x; 
         return n > 0 ? half: 1/half; 
     }
 }
