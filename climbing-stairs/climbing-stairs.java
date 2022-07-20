53fbe8e5180c1ef58f1279f36830800b1f99ef7f
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-19 23:17:13 -0500

    Time: 0 ms, Memory: 40.6 MB - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,14 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        
-        dp[0] = 1;
-        dp[1] = 1;
+        int a = 1;
+        int b = 1;
+        int c = 1; 
         
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            c = a + b; 
+            a = b; 
+            b = c; 
         }
         
-        return dp[n];
+        return c;
     }
 }
