eb2557e763c7a127d6c1b7d275d9b3ce801be11b
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-25 09:59:14 -0500

    Time: 0 ms (100.00%), Space: 40.3 MB (94.33%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -16,15 +16,34 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList(); 
-        preorderTraversal(root, list);
+        Deque<TreeNode> stack = new ArrayDeque(); 
+        
+        if(root != null)
+            stack.push(root); 
+        
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop(); 
+            list.add(current.val);
+            if(current.right != null)
+                stack.push(current.right);
+            if(current.left != null)
+                stack.push(current.left); 
+        }
+        
         return list; 
     }
+
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList(); 
+//         preorderTraversal(root, list);
+//         return list; 
+//     }
     
-    public void preorderTraversal(TreeNode node, List<Integer> list){
-        if(node == null)
-            return;
-        list.add(node.val);
-        preorderTraversal(node.left, list);
-        preorderTraversal(node.right, list);
-    }
+//     public void preorderTraversal(TreeNode node, List<Integer> list){
+//         if(node == null)
+//             return;
+//         list.add(node.val);
+//         preorderTraversal(node.left, list);
+//         preorderTraversal(node.right, list);
+//     }
 }
