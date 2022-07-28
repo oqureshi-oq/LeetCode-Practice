bb31e1e6b28c89f70c1e584b207e3eef311108ce
Omar Qureshi <oqureshi.oq@gmail.com>
2022-07-27 20:53:06 -0500

    Time: 29 ms (13.95%), Space: 48.3 MB (21.18%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        if(board == null) return false; 
+        if(board == null)
+            return false; 
         
-        Set<String> seen = new HashSet(); 
+        Set<String> set = new HashSet(); 
         
         for(int row = 0; row < board.length; row++){
             for(int col = 0; col < board[row].length; col++){
-                char value = board[row][col]; 
-                
-                if(value == '.') 
+                if(board[row][col] == '.')
                     continue; 
                 
-                if(!seen.add("row" + row + "v" + value) || 
-                   !seen.add("col" + col + "v" + value) ||
-                   !seen.add("rowSquare" + row / 3 + "colSquare" + col / 3 + "v" + value))
+                if(!set.add("row:"+row+",value:"+board[row][col]) ||
+                   !set.add("col:"+col+",value:"+board[row][col]) ||
+                   !set.add("boxrow:"+row/3+",boxcol:"+col/3+",value:"+board[row][col]))
                     return false; 
             }
         }
