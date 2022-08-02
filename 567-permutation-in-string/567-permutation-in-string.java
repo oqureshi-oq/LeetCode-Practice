c6a8f5ea9bdbdb04ef83722b9b2b57abfcd55681
Omar Qureshi <oqureshi.oq@gmail.com>
2022-08-01 20:30:55 -0500

    Time: 16 ms (46.24%), Space: 42.2 MB (92.47%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1 == null || s2 == null)
-            return false;
+            return false; 
         
         Map<Character, Integer> map = new HashMap(); 
         
@@ -9,30 +9,33 @@ class Solution {
             map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1); 
         }
         
-        int left = 0;
-        int right = 0;
+        int left = 0; 
+        int right = 0; 
+        int count = 0; 
         
         while(right < s2.length()){
-            if(map.containsKey(s2.charAt(right))){
-                map.put(s2.charAt(right), map.getOrDefault(s2.charAt(right), 0) - 1);
+            if(map.getOrDefault(s2.charAt(right), 0) > 0){
+                map.put(s2.charAt(right), map.get(s2.charAt(right)) - 1); 
+                count++;  
+                right++; 
                 
-                while(map.get(s2.charAt(right)) < 0){
-                    if(map.containsKey(s2.charAt(left)))
-                        map.put(s2.charAt(left), map.getOrDefault(s2.charAt(left), 0) + 1);
-                    left++; 
-                }
-                
-                if(right - left + 1 == s1.length())
+                if(count == s1.length())
                     return true; 
             } else {
-                while(left != right+1){
-                    if(map.containsKey(s2.charAt(left)))
-                        map.put(s2.charAt(left), map.getOrDefault(s2.charAt(left), 0) + 1);
+                while(left != right+1 && map.getOrDefault(s2.charAt(right), 0) <= 0){
+                    if(map.containsKey(s2.charAt(left))){
+                        count--;
+                        map.put(s2.charAt(left), map.get(s2.charAt(left)) + 1); 
+                    } 
+                    
                     left++; 
                 }
+                
+                if(left == right+1)
+                    right++; 
             }
             
-            right++; 
+            
         }
         
         return false; 
