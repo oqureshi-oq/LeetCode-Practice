76ad4d83f494f6014f4826af9c1039750b7bc372
Omar Qureshi <oqureshi.oq@gmail.com>
2022-08-02 20:13:53 -0500

    Time: 2 ms (93.63%), Space: 59 MB (91.64%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -3,13 +3,13 @@ class Solution {
         if(prices == null || prices.length < 2)
             return 0;
         
-        int buy = prices[0];
         int maxProfit = 0; 
+        int buyPrice = prices[0];
         
         for(int i = 1; i < prices.length; i++){
-            maxProfit = Math.max(prices[i] - buy, maxProfit);
-            if(prices[i] < buy)
-                buy = prices[i];
+            maxProfit = Math.max(prices[i] - buyPrice, maxProfit); 
+            if(prices[i] < buyPrice)
+                buyPrice = prices[i];
         }
         
         return maxProfit; 
