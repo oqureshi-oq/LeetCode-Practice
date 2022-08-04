9e5557e8ff0c1bd47194ae0cf6d929db2a9915f5
Omar Qureshi <oqureshi.oq@gmail.com>
2022-08-03 23:43:05 -0500

    Time: 24 ms (49.39%), Space: 45.2 MB (50.84%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -3,55 +3,64 @@ class Solution {
         if(s == null || t == null)
             return ""; 
         
+        Map<Character, Integer> map = new HashMap(); 
         int left = 0;
         int right = 0;
-        int min = s.length() + 1; 
-        int leftAns = 0;
+        int min = s.length()+1; 
+        int leftAns = -1;
         int rightAns = -1;
-        int count = 0;
-        Map<Character, Integer> map = new HashMap(); 
+        int count = 0; 
         
         for(int i = 0; i < t.length(); i++){
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1); 
         }
         
         while(right < s.length()){
+            
             if(map.containsKey(s.charAt(right))){
-                map.put(s.charAt(right), map.get(s.charAt(right)) - 1); 
+                if(map.get(s.charAt(right)) > 0)
+                    count++;
                 
-                if(map.get(s.charAt(right)) >= 0)
-                    count++; 
+                map.put(s.charAt(right), map.get(s.charAt(right)) - 1); 
             }
             
-            while(left < s.length() && 
+            while(left <= right && 
                   (!map.containsKey(s.charAt(left)) || map.get(s.charAt(left)) < 0)){
-                if(map.getOrDefault(s.charAt(left), 0) < 0)
+                
+                if(map.containsKey(s.charAt(left)))
                     map.put(s.charAt(left), map.get(s.charAt(left)) + 1); 
-                left++;
+                
+                left++; 
             }
             
             if(count == t.length()){
+                
                 if(right-left+1 < min){
-                    min = right-left+1; 
-                    leftAns = left; 
+                    leftAns = left;
                     rightAns = right; 
+                    min = right-left+1;
                 }
                 
-                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
-                count--; 
-                left++; 
-                
-                while(left < s.length() &&
-                      (!map.containsKey(s.charAt(left)) || map.get(s.charAt(left)) < 0)){
-                    if(map.getOrDefault(s.charAt(left), 0) < 0)
+                while(count == t.length()){
+                    
+                    if(map.containsKey(s.charAt(left))){
+                        
+                        if(map.get(s.charAt(left)) == 0)
+                            count--; 
+                        
                         map.put(s.charAt(left), map.get(s.charAt(left)) + 1); 
-                    left++;
+                    }
+                    
+                    left++; 
                 }
             }
             
             right++; 
         }
         
-        return s.substring(leftAns, rightAns+1); 
+        if(leftAns > -1 && rightAns > -1)
+            return s.substring(leftAns, rightAns+1); 
+        
+        return "";
     }
 }
