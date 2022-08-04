1ddfab4e03a35052220fb3f10332bed5819a49b0
Omar Qureshi <oqureshi.oq@gmail.com>
2022-08-03 22:28:41 -0500

    Time: 26 ms (39.19%), Space: 44.1 MB (28.21%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -3,39 +3,43 @@ class Solution {
         if(s1 == null || s2 == null)
             return false; 
         
+        int left = 0;
+        int right = 0; 
+        int count = 0;
         Map<Character, Integer> map = new HashMap(); 
         
         for(int i = 0; i < s1.length(); i++){
-            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1); 
+            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);  
         }
         
-        int left = 0; 
-        int right = 0; 
-        int count = 0; 
-        
         while(right < s2.length()){
-            if(map.getOrDefault(s2.charAt(right), 0) > 0){
-                map.put(s2.charAt(right), map.get(s2.charAt(right)) - 1); 
-                count++;  
-                right++; 
+            if(!map.containsKey(s2.charAt(right))){
                 
-                if(count == s1.length())
-                    return true; 
-            } else {
-                while(left != right+1 && map.getOrDefault(s2.charAt(right), 0) <= 0){
-                    if(map.containsKey(s2.charAt(left))){
-                        count--;
+                while(left != right+1){
+                    if(map.containsKey(s2.charAt(left)))
                         map.put(s2.charAt(left), map.get(s2.charAt(left)) + 1); 
-                    } 
-                    
                     left++; 
                 }
                 
-                if(left == right+1)
-                    right++; 
+                count = 0; 
+                
+            } else {
+                while(map.get(s2.charAt(right)) == 0){
+                    if(map.containsKey(s2.charAt(left))){
+                        map.put(s2.charAt(left), map.get(s2.charAt(left)) + 1); 
+                        count--; 
+                    }
+                    left++; 
+                }
+                
+                map.put(s2.charAt(right), map.get(s2.charAt(right)) - 1); 
+                count++; 
+                
+                if(count == s1.length())
+                    return true; 
             }
             
-            
+            right++; 
         }
         
         return false; 
