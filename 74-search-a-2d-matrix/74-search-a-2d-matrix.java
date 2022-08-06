3497f62244b3e4a9ce35f9edef07d792eab8a78b
Omar Qureshi <oqureshi.oq@gmail.com>
2022-08-06 12:48:51 -0500

    Time: 0 ms (100.00%), Space: 43.1 MB (17.09%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -3,32 +3,23 @@ class Solution {
         if(matrix == null)
             return false; 
         
-        int leftRow = 0;
-        int rightRow = matrix.length - 1; 
+        int m = matrix.length; 
+        int n = matrix[0].length; 
         
-        while(leftRow <= rightRow){
-            int midRow = leftRow + (rightRow - leftRow)/2; 
+        int left = 0;
+        int right = m*n-1; 
+        
+        while(left <= right){
+            int mid = left + (right - left)/2; 
             
-            if(matrix[midRow][0] <= target && target <= matrix[midRow][matrix[midRow].length-1]){
-                int leftCol = 0;
-                int rightCol = matrix[midRow].length - 1;
-                
-                while(leftCol <= rightCol){
-                    int midCol = leftCol + (rightCol - leftCol)/2; 
-                    
-                    if(matrix[midRow][midCol] == target)
-                        return true;
-                    else if(matrix[midRow][midCol] < target)
-                        leftCol = midCol + 1;
-                    else
-                        rightCol = midCol - 1;
-                }
-                
-                return false; 
-            } else if(target < matrix[midRow][0])
-                rightRow = midRow - 1;
+            int value = matrix[mid / n][mid % n];
+            
+            if(value == target)
+                return true;
+            else if(value < target)
+                left = mid+1;
             else
-                leftRow = midRow + 1; 
+                right = mid-1; 
         }
         
         return false; 
