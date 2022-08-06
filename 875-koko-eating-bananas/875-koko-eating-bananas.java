926d489cdcc7058c19092f831b2b84a67bf29d2f
Omar Qureshi <oqureshi.oq@gmail.com>
2022-08-06 15:10:24 -0500

    Time: 18 ms (89.42%), Space: 43.4 MB (86.00%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -3,12 +3,15 @@ class Solution {
         if(piles == null)
             return -1; 
         
-        int left = 1; 
+        int left = piles[0]; 
         int right = piles[0];
                 
         for(int n: piles){
             right = Math.max(right, n); 
+            left = Math.min(left, n); 
         }
+        
+        left = left % h == 0 ? left/h: left/h + 1; 
         
         while(left < right){
             int mid = left + (right - left)/2; 
