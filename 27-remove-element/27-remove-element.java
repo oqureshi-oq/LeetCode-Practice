cdd535955ff1d2ce53b7ddbeb60ed80b68c37bdf
Omar Qureshi <oqureshi.oq@gmail.com>
2022-09-20 14:14:55 -0500

    Time: 1 ms (38.33%), Space: 42.5 MB (54.17%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,11 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        if(nums == null) return -1;
+        if(nums == null)
+            return -1; 
         
         int k = 0;
+        
         for(int n: nums){
             if(n != val)
-                nums[k++] = n;
+                nums[k++] = n; 
         }
         
         return k; 
