fd6e02918658fdcfaa7e4084adabf49fa9ae26c4
Omar Qureshi <oqureshi.oq@gmail.com>
2022-09-21 11:18:45 -0500

    Time: 5 ms (61.40%), Space: 43.5 MB (66.28%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -1,28 +1,27 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        if(nums == null) return -1;
+        if(nums == null)
+            return -1; 
         
-        Integer first = null; 
+        Integer first = null;
         Integer second = null; 
         Integer third = null; 
         
         for(Integer n: nums){
             if(n.equals(first) || n.equals(second) || n.equals(third))
-                continue;
+                continue; 
             
             if(first == null || n > first){
-                third = second;
-                second = first;
-                first = n;
+                third = second; 
+                second = first; 
+                first = n; 
             } else if(second == null || n > second){
                 third = second;
-                second = n;
+                second = n; 
             } else if(third == null || n > third){
                 third = n; 
             }
         }
-        
-        if(first == null) return -1;
         
         return third != null ? third: first; 
     }
