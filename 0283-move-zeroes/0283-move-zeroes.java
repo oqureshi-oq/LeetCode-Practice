db7183a827fa2caa6fd1ad93ff09e4d094055b53
Omar Qureshi <oqureshi.oq@gmail.com>
2022-10-10 13:19:21 -0500

    Time: 3 ms (58.54%), Space: 54.5 MB (65.42%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null)
+            return; 
+        
+        int write = 0;
+        
+        for(int n: nums){
+            if(n != 0)
+                nums[write++] = n; 
+        }
+        
+        while(write < nums.length)
+            nums[write++] = 0; 
+    }
+}
