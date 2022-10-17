0d07d4250f02162981759a52c8da78f7e0e4b3dd
Omar Qureshi <oqureshi.oq@gmail.com>
2022-10-17 17:06:26 -0500

    Time: 24 ms (26.77%), Space: 54 MB (53.18%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if(nums == null || nums.length == 0 || nums.length % 2 != 0)
+            return Integer.MIN_VALUE; 
+        
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        
+        for(int i = 0; i < nums.length; i += 2){
+            sum += nums[i];
+        }
+        
+        return sum; 
+    }
+}
