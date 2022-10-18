f8211e6805c5d70e9d039035372be87e622769ab
Omar Qureshi <oqureshi.oq@gmail.com>
2022-10-18 12:13:47 -0500

    Time: 16 ms (65.35%), Space: 68.3 MB (70.72%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums == null)
+            return false; 
+        
+        Set<Integer> seen = new HashSet(); 
+        
+        for(int n: nums){
+            if(seen.contains(n))
+                return true; 
+            seen.add(n); 
+        }
+        
+        return false; 
+    }
+}
