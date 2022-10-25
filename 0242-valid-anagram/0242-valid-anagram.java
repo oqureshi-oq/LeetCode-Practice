d3c2634dad5340ddda4d53155f4447f98cd39eb0
Omar Qureshi <oqureshi.oq@gmail.com>
2022-10-25 13:55:41 -0500

    Time: 6 ms (78.24%), Space: 43.5 MB (69.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,18 +3,17 @@ class Solution {
         if(s == null || t == null || s.length() != t.length())
             return false; 
         
-        Map<Character, Integer> mapS = new HashMap(); 
-        Map<Character, Integer> mapT = new HashMap(); 
+        int[] map = new int[26]; 
+        int n = s.length(); 
         
-        for(int i = 0; i < s.length(); i++){
-            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1); 
-            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1); 
+        for(int i = 0; i < n; i++){
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
         }
         
-        for(Character c: mapS.keySet()){
-            if(!mapS.get(c).equals(mapT.getOrDefault(c, 0))){ 
+        for(int x: map){
+            if(x != 0)
                 return false; 
-            }
         }
         
         return true; 
