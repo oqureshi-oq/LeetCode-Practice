3792630ee39cf1cbb7044b1c3fca9800e5ccf111
Omar Qureshi <oqureshi.oq@gmail.com>
2022-10-29 15:28:41 -0500

    Time: 55 ms (12.56%), Space: 42.9 MB (65.66%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s == null || t == null || s.length() != t.length())
-            return false;
+            return false; 
         
-        return transformString(s).compareTo(transformString(t)) == 0; 
-    }
-    
-    public String transformString(String s){
-        if(s == null)
-            return new String(); 
-        
-        Map<Character, Integer> map = new HashMap(); 
-        StringBuilder str = new StringBuilder(); 
+        Map<Character, Character> sTot = new HashMap(); 
+        Map<Character, Character> tTos = new HashMap(); 
         
         for(int i = 0; i < s.length(); i++){
-            if(!map.containsKey(s.charAt(i)))
-                map.put(s.charAt(i), i); 
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i); 
             
-            str.append(map.get(s.charAt(i))); 
-            str.append(' '); 
+            if(sTot.containsKey(sChar) && tTos.containsKey(tChar)){
+                if(sTot.get(sChar) != tChar || tTos.get(tChar) != sChar)
+                    return false; 
+            } else if(sTot.containsKey(sChar) || tTos.containsKey(tChar)){
+                return false; 
+            } else {
+                sTot.put(sChar, tChar); 
+                tTos.put(tChar, sChar); 
+            }
         }
-
-        return str.toString(); 
+        
+        return true; 
     }
 }
