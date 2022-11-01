8e25c9f1ac312b83ae498f558e68f9f3e03998ac
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-01 10:13:32 -0500

    Time: 4 ms (9.40%), Space: 52.1 MB (56.94%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(flowerbed == null || flowerbed.length < n)
+            return false; 
+        
+        for(int i = 0; i < flowerbed.length; i++){
+            if(flowerbed[i] == 1)
+                continue; 
+            
+            boolean isLeftEmpty = i == 0 ? true: flowerbed[i-1] == 0;
+            boolean isRightEmpty = i == flowerbed.length-1 ? true: flowerbed[i+1] == 0;
+            
+            if(isLeftEmpty && isRightEmpty){
+                flowerbed[i] = 1; 
+                n--; 
+            }
+        }
+        
+        return n <= 0; 
+    }
+}
