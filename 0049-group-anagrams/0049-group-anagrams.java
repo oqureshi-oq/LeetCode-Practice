4b144e8a903a6e9c29eaa19c6b47ac4342aac433
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-08 18:52:40 -0600

    Time: 14 ms (62.48%), Space: 56.3 MB (38.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,13 +6,13 @@ class Solution {
         Map<String, List<String>> map = new HashMap(); 
         
         for(String str: strs){
-            char[] letters = new char[26];
+            char[] letters = new char[26]; 
             for(int i = 0; i < str.length(); i++){
                 letters[str.charAt(i) - 'a']++; 
             }
-            String key = String.valueOf(letters);
+            String key = String.valueOf(letters); 
             if(!map.containsKey(key))
-                map.put(key, new ArrayList()); 
+                map.put(key, new ArrayList());
             map.get(key).add(str); 
         }
         
