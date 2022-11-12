dc022c28096b4b23b8d69caaa87d1585878d3476
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-11 19:58:14 -0600

    Time: 6 ms (76.82%), Space: 44.1 MB (54.39%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap();
+        Deque<Integer> deque = new ArrayDeque(); 
+        
+        for(int i = 0; i < nums2.length; i++){
+            while(!deque.isEmpty() && nums2[deque.getFirst()] < nums2[i]){
+                nums2[deque.pollFirst()] = nums2[i]; 
+            }
+            
+            map.put(nums2[i], i); 
+            deque.addFirst(i); 
+        }
+        
+        while(!deque.isEmpty())
+            nums2[deque.pollFirst()] = -1; 
+        
+        for(int i = 0; i< nums1.length; i++){
+            nums1[i] = nums2[map.get(nums1[i])]; 
+        }
+        
+        return nums1; 
+    }
+}
+
+
