544ea3604f3f5cc968eabaf6a2bfb2a2ba140e6a
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-14 10:59:46 -0600

    Time: 0 ms (100.00%), Space: 41.9 MB (74.85%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         if(s == null)
-            return 0; 
+            return 0;
         
-        int i = s.length()-1; 
-        int length = 0; 
+        int i = s.length()-1;
+        int length = 0;
         
         while(i >= 0 && s.charAt(i) == ' ')
-            i--; 
+            i--;
         
         while(i >= 0 && s.charAt(i) != ' '){
             length++;
