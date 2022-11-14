7f13d25ca63861d17abf2f24ecc465674cc5f11c
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-14 14:53:54 -0600

    Time: 94 ms (5.59%), Space: 42.9 MB (59.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,20 +5,19 @@ class Solution {
         
         Map<Character, Character> stMap = new HashMap(); 
         Map<Character, Character> tsMap = new HashMap(); 
-        int n = s.length(); 
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < s.length(); i++){
             char sChar = s.charAt(i);
-            char tChar = t.charAt(i); 
+            char tChar = t.charAt(i);
             
             if(stMap.containsKey(sChar) && stMap.get(sChar) != tChar)
                 return false; 
             
             if(tsMap.containsKey(tChar) && tsMap.get(tChar) != sChar)
-                return false; 
+                return false;
             
             stMap.put(sChar, tChar);
-            tsMap.put(tChar, sChar); 
+            tsMap.put(tChar, sChar);
         }
         
         return true; 
