18c398ff0cd0aff5e3252a1532cd0d2f1c323f29
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-16 14:34:36 -0600

    Time: 34 ms (39.48%), Space: 50.9 MB (29.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         if(nums == null || k > nums.length)
-            return new int[0]; 
+            return new int[0];
         
-        HashMap<Integer, Integer> map = new HashMap(); 
+        Map<Integer, Integer> map = new HashMap(); 
         
-        for(int n: nums){
-            map.put(n, map.getOrDefault(n, 0) + 1); 
+        for(int n:nums){
+            map.put(n, map.getOrDefault(n,0)+1); 
         }
         
-        ArrayList<Integer>[] arr = new ArrayList[nums.length+1]; 
+        List[] list = new List[nums.length+1]; 
         
         for(int n: map.keySet()){
-            if(arr[map.get(n)] == null)
-                arr[map.get(n)] = new ArrayList(); 
-            
-            arr[map.get(n)].add(n); 
+            if(list[map.get(n)] == null)
+                list[map.get(n)] = new ArrayList(); 
+            list[map.get(n)].add(n); 
         }
         
-        int[] ans = new int[k]; 
+        int[] ans = new int[k];
+        k--;
+        int i = list.length-1; 
         
-        for(int i = nums.length; k > 0 && i >= 0; i--){
-            if(arr[i] == null)
-                continue; 
+        while(k >= 0 && i >= 0){
             
-            for(int j = 0; k > 0 && j < arr[i].size(); j++){
-                ans[k-1] = arr[i].get(j); 
-                k--; 
+            for(int j = 0; k >= 0 && list[i] != null && j < list[i].size(); j++){
+                ans[k--] = (int) list[i].get(j); 
             }
+            
+            i--; 
         }
         
         return ans; 
