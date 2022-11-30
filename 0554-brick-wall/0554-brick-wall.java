7691e6706dd202b4464cb323cfa605ce535f3f2f
Omar Qureshi <oqureshi.oq@gmail.com>
2022-11-29 20:45:52 -0600

    Time: 54 ms (5.23%), Space: 55 MB (54.55%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.java
+++ 0554-brick-wall/0554-brick-wall.java
@@ -4,6 +4,7 @@ class Solution {
             return 0;
         
         Map<Integer, Integer> map = new HashMap(); 
+        int max = 0; 
         
         for(int i = 0; i < wall.size(); i++){
             if(wall.get(i) == null)
@@ -14,15 +15,11 @@ class Solution {
             for(int j = 0; j < wall.get(i).size()-1; j++){
                 edgeAccumulator += wall.get(i).get(j); 
                 map.put(edgeAccumulator, map.getOrDefault(edgeAccumulator,0) + 1); 
+                max = Math.max(max, map.get(edgeAccumulator)); 
             }
              
         }
         
-        int max = 0; 
-        
-        for(int edge: map.keySet()){
-            max = Math.max(max, map.get(edge));
-        }
         
         return wall.size()-max; 
     }
