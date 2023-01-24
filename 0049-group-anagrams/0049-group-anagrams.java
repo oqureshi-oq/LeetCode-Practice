992dd7757c450b50589761c8d821aa4b159b2720
Omar Qureshi <oqureshi.oq@gmail.com>
2023-01-24 07:45:51 -0600

    Time: 15 ms (39.32%), Space: 56.6 MB (16.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,25 @@
 class Solution {
+    final int NUMBER_OF_LETTERS = 26; 
+    
     public List<List<String>> groupAnagrams(String[] strs) {
         if(strs == null)
             return new ArrayList(); 
         
-        Map<String, List<String>> map = new HashMap(); 
+        Map<String, List> map = new HashMap();
         
         for(String str: strs){
-            char[] letters = new char[26];
+            char[] letters = new char[NUMBER_OF_LETTERS]; 
+            
             for(int i = 0; i < str.length(); i++){
                 letters[str.charAt(i) - 'a']++; 
             }
-            String key = String.valueOf(letters); 
+            
+            String key = new String(letters);
+            
             if(!map.containsKey(key))
                 map.put(key, new ArrayList()); 
-            map.get(key).add(str);
+            
+            map.get(key).add(str); 
         }
         
         return new ArrayList(map.values()); 
