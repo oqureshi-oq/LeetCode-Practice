b23765d099d52549a380bc52b19678ae8c4a3773
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-01 06:38:57 -0600

    Time: 3 ms (84.28%), Space: 42.1 MB (96.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,11 +5,11 @@ class Solution {
         if(s == null || t == null || s.length() != t.length())
             return false; 
         
-        int[] letters = new int[NUMBER_OF_LETTERS]; 
+        int[] letters = new int[NUMBER_OF_LETTERS];
         
         for(int i = 0; i < s.length(); i++){
             letters[s.charAt(i) - 'a']++;
-            letters[t.charAt(i) - 'a']--; 
+            letters[t.charAt(i) - 'a']--;
         }
         
         for(int n: letters){
