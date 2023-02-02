cb60173ddee59f54e9891095a44737467892c184
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-02 17:34:29 -0600

    Time: 230 ms (34.48%), Space: 50.1 MB (38.96%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,20 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i)
-            if (dups.add(nums[i])) {
-                for (int j = i + 1; j < nums.length; ++j) {
-                    int complement = -nums[i] - nums[j];
-                    if (seen.containsKey(complement) && seen.get(complement) == i) {
-                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                        Collections.sort(triplet);
-                        res.add(triplet);
-                    }
-                    seen.put(nums[j], i);
-                }
+        if(nums == null)
+            return new ArrayList(); 
+        
+        Set<List<Integer>> ans = new HashSet(); 
+        Map<Integer, Integer> map = new HashMap();
+        Set<Integer> seen = new HashSet(); 
+        
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i); 
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!seen.add(nums[i]))
+                continue; 
+            
+            for(int j = i+1; j < nums.length; j++){               
+                int sum = nums[i] + nums[j]; 
+                
+                if(map.containsKey(-sum) && j < map.get(-sum)){
+                    List<Integer> list = new ArrayList(); 
+                    
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(-sum); 
+                    
+                    Collections.sort(list); 
+                    
+                    ans.add(list); 
+                }                
             }
-        return new ArrayList(res);
+        }
+        
+        return new ArrayList(ans); 
     }
 }
