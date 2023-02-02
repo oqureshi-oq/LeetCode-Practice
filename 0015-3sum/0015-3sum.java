a66b701a56bf8ea90dea12f31145e07a8f2c84a6
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-02 13:40:56 -0600

    Time: 936 ms (11.56%), Space: 50.8 MB (31.78%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if(nums == null)
+            return new ArrayList(); 
+        
+        Set<List<Integer>> ans = new HashSet(); 
+        Map<Integer, Integer> map = new HashMap(); 
+        
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i); 
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                int sum = nums[i] + nums[j]; 
+                if(map.containsKey(-sum) && j < map.get(-sum)){
+                    List<Integer> list = new ArrayList(); 
+                    
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(-sum); 
+                    
+                    Collections.sort(list); 
+                    
+                    ans.add(list); 
+                }                
+            }
+        }
+        
+        return new ArrayList(ans); 
+    }
+}
