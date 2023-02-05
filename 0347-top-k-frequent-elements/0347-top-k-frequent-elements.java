4a429d7fd506c3caacd1c28215b7f4334aeb12ca
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-05 07:00:55 -0600

    Time: 8 ms (98.19%), Space: 44.4 MB (98.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,32 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if(nums == null || nums.length < k || k <= 0)
+        if(nums == null)
             return new int[0];
         
-        Map<Integer, Integer> map = new HashMap(); 
+        Map<Integer, Integer> counts = new HashMap(); 
         
         for(int n: nums){
-            map.put(n, map.getOrDefault(n,0) + 1); 
+            counts.put(n, counts.getOrDefault(n, 0) + 1); 
         }
         
         List<Integer>[] buckets = new List[nums.length+1]; 
         
-        for(int n: map.keySet()){
-            int count = map.get(n);
+        for(int n: counts.keySet()){
+            int count = counts.get(n);
             
             if(buckets[count] == null)
                 buckets[count] = new ArrayList(); 
             
-            buckets[count].add(n);
+            buckets[count].add(n); 
         }
         
         int[] ans = new int[k]; 
         
-        for(int i = buckets.length-1; i > 0 && k > 0; i--){
+        for(int i = buckets.length-1; k > 0 && i >= 0; i--){
             if(buckets[i] == null)
-                continue; 
+                continue;
             
-            for(int j = 0; j < buckets[i].size() && k > 0; j++){
+            for(int j = 0; k > 0 && j < buckets[i].size(); j++){
                 ans[k-1] = buckets[i].get(j); 
                 k--; 
             }
