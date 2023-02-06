e0e9fff7f1b97c2c34272057ce864af4eca1f040
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-06 12:58:48 -0600

    Time: 3 ms (23.41%), Space: 43.4 MB (39.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,41 +3,24 @@ class Solution {
         if(height == null)
             return 0;
         
-        int left = 0;
-        int right = 0; 
-        int rain = 0; 
+        Deque<Integer> stack = new ArrayDeque(); 
+        int current = 0;
+        int rain = 0;
         
-        while(right < height.length){
-            while(right < height.length && (left == right || height[left] > height[right]))
-                right++;
-            
-            if(right == height.length)
-                break; 
-            
-            int wallHeight = height[left];
-            
-            while(left < right){
-                rain += wallHeight - height[left];
-                left++;
+        while(current < height.length){
+            while(!stack.isEmpty() && height[stack.peek()] <= height[current]){
+                int middle = stack.pop(); 
+                
+                if(stack.isEmpty())
+                    break;
+                
+                int h = Math.min(height[stack.peek()], height[current]) - height[middle]; 
+                int l = current - stack.peek() - 1; 
+                
+                rain += h*l; 
             }
-        }
-        
-        int leftEnd = left;
-        left = right = height.length-1; 
-        
-        while(leftEnd <= left){
-            while(leftEnd <= left && (left == right || height[left] < height[right]))
-                left--;
             
-            if(left < leftEnd)
-                break; 
-            
-            int wallHeight = height[right];
-            
-            while(left < right){
-                rain += wallHeight - height[right];
-                right--; 
-            }
+            stack.push(current++); 
         }
         
         return rain; 
