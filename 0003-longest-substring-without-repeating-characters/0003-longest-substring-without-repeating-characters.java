687386deb8757b8b66a862acd828495dc2682959
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-06 16:20:32 -0600

    Time: 7 ms (70.96%), Space: 42.1 MB (91.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,20 +3,24 @@ class Solution {
         if(s == null)
             return 0;
         
-        Set<Character> seen = new HashSet(); 
-        int max = 0; 
+        Set<Character> seen = new HashSet();
+        int maxLength = 0;  
+        int left = 0;
+        int right = 0;
         
-        for(int left = 0, right = 0; right < s.length(); right++){
-            char c = s.charAt(right); 
+        while(right < s.length()){
+            char c = s.charAt(right);
             
             while(seen.contains(c))
                 seen.remove(s.charAt(left++)); 
             
             seen.add(c); 
             
-            max = Math.max(max, right - left + 1); 
+            maxLength = Math.max(maxLength, right - left + 1); 
+            
+            right++; 
         }
         
-        return max; 
+        return maxLength; 
     }
 }
