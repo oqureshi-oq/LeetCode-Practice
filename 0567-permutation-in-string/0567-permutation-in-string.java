7206e065c0f6cf70cc3abaf30bc92ed18eea4e73
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-07 00:12:16 -0600

    Time: 18 ms (41.11%), Space: 41.9 MB (92.10%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,37 +1,36 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        int[] s1map = new int[26];
-        int[] s2map = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1map[s1.charAt(i) - 'a']++;
-            s2map[s2.charAt(i) - 'a']++;
+        if(s1 == null || s2 == null || s1.length() > s2.length())
+            return false; 
+        
+        Map<Character, Integer> map1 = new HashMap(); 
+        
+        for(int i = 0; i < s1.length(); i++){
+            char c = s1.charAt(i); 
+            map1.put(c, map1.getOrDefault(c, 0) + 1); 
         }
-
+        
+        Map<Character, Integer> map2 = new HashMap(); 
         int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1map[i] == s2map[i])
-                count++;
-        }
-
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
-            if (count == 26)
-                return true;
-            s2map[r]++;
-            if (s2map[r] == s1map[r]) {
-                count++;
-            } else if (s2map[r] == s1map[r] + 1) {
-                count--;
+        int left = 0;
+        int right = 0;
+        
+        while(right < s2.length()){
+            char c = s2.charAt(right); 
+            
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+            
+            while(map2.get(c) > map1.getOrDefault(c, 0)){
+                map2.put(s2.charAt(left), map2.get(s2.charAt(left)) - 1);
+                left++; 
             }
-            s2map[l]--;
-            if (s2map[l] == s1map[l]) {
-                count++;
-            } else if (s2map[l] == s1map[l] - 1) {
-                count--;
-            }
+            
+            if(right - left + 1 == s1.length())
+                return true; 
+            
+            right++; 
         }
-        return count == 26;
+        
+        return false; 
     }
 }
