c864a4fb0618d6139e18465ffa8fc881f7371f6a
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-07 00:14:01 -0600

    Time: 4 ms (86.35%), Space: 41.8 MB (92.10%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,14 +3,14 @@ class Solution {
         if(s1 == null || s2 == null || s1.length() > s2.length())
             return false; 
         
-        Map<Character, Integer> map1 = new HashMap(); 
+        int[] letters1 = new int[26];  
         
         for(int i = 0; i < s1.length(); i++){
             char c = s1.charAt(i); 
-            map1.put(c, map1.getOrDefault(c, 0) + 1); 
+            letters1[c - 'a']++;  
         }
         
-        Map<Character, Integer> map2 = new HashMap(); 
+        int[] letters2 = new int[26];   
         int count = 0;
         int left = 0;
         int right = 0;
@@ -18,10 +18,10 @@ class Solution {
         while(right < s2.length()){
             char c = s2.charAt(right); 
             
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
+            letters2[c - 'a']++; 
             
-            while(map2.get(c) > map1.getOrDefault(c, 0)){
-                map2.put(s2.charAt(left), map2.get(s2.charAt(left)) - 1);
+            while(letters2[c - 'a'] > letters1[c - 'a']){
+                letters2[s2.charAt(left) - 'a']--; 
                 left++; 
             }
             
