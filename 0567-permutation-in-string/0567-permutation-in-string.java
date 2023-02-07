fab8fcdb896c9cc5deea5c33ebbba7febee9b44f
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-07 11:31:05 -0600

    Time: 4 ms (86.35%), Space: 42 MB (85.29%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,30 @@
 class Solution {
+    final int NUMBER_OF_LETTERS = 26; 
+    
     public boolean checkInclusion(String s1, String s2) {
         if(s1 == null || s2 == null || s1.length() > s2.length())
             return false; 
         
-        int[] letters1 = new int[26];  
+        int[] letters1 = new int[NUMBER_OF_LETTERS];
         
         for(int i = 0; i < s1.length(); i++){
-            char c = s1.charAt(i); 
-            letters1[c - 'a']++;  
+            letters1[s1.charAt(i) - 'a']++; 
         }
         
-        int[] letters2 = new int[26];   
-        int count = 0;
         int left = 0;
         int right = 0;
+        int[] letters2 = new int[NUMBER_OF_LETTERS]; 
         
         while(right < s2.length()){
-            char c = s2.charAt(right); 
+            char c = s2.charAt(right);
             
-            letters2[c - 'a']++; 
+            letters2[c - 'a']++;
             
-            while(letters2[c - 'a'] > letters1[c - 'a']){
-                letters2[s2.charAt(left) - 'a']--; 
-                left++; 
-            }
+            while(letters2[c - 'a'] > letters1[c - 'a'])
+                letters2[s2.charAt(left++) - 'a']--; 
             
             if(right - left + 1 == s1.length())
-                return true; 
+                return true;
             
             right++; 
         }
