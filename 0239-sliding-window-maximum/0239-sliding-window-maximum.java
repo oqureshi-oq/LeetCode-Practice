6aa1f13bc76255ad81244813eeb55e2f36074927
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-08 11:54:47 -0600

    Time: 47 ms (39.75%), Space: 57.8 MB (91.51%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -4,25 +4,25 @@ class Solution {
             return new int[0];
         
         Deque<Integer> deque = new ArrayDeque(); 
-        List<Integer> list = new ArrayList();
-            
+        List<Integer> list = new ArrayList(); 
+        
         for(int i = 0; i < nums.length; i++){
-            while(!deque.isEmpty() && nums[deque.getLast()] <= nums[i])
-                deque.pollLast();  
+            while(!deque.isEmpty() && nums[deque.peekLast()] <= nums[i])
+                deque.pollLast(); 
             
-            while(!deque.isEmpty() && i - k >= deque.peek())
-                deque.poll(); 
+            while(!deque.isEmpty() && i - k >= deque.peekFirst())
+                deque.pollFirst(); 
             
-            deque.add(i);
-                        
-            if(!deque.isEmpty() && i+1 >= k)
-                list.add(nums[deque.peek()]);
+            deque.addLast(i);
+            
+            if(i+1 >= k)
+                list.add(nums[deque.getFirst()]); 
         }
         
         int[] ans = new int[list.size()];
         
         for(int i = 0; i < list.size(); i++){
-            ans[i] = list.get(i);
+            ans[i] = list.get(i); 
         }
         
         return ans; 
