3321f86ab7eb3b2f7fb821ac3ae67caab1037c0f
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-10 12:31:10 -0600

    Time: 0 ms (100.00%), Space: 42.1 MB (92.68%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,30 +2,27 @@ class Solution {
     List<String> list; 
     
     public List<String> generateParenthesis(int n) {
-        if(n <= 0)
-            return new ArrayList(); 
-        
-        list = new ArrayList(); 
+        list = new ArrayList();
         generateParenthesis(n, 0, 0, new StringBuilder()); 
         return list; 
     }
     
-    public void generateParenthesis(int n, int open, int close, StringBuilder sb){
-        if(n == close){
+    private void generateParenthesis(int n, int open, int close, StringBuilder sb){
+        if(close == n){
             list.add(sb.toString());
-            return; 
+            return;
         }
         
         if(open < n){
             sb.append('(');
-            generateParenthesis(n, open+1, close, sb); 
+            generateParenthesis(n, open + 1, close, sb);
             sb.deleteCharAt(sb.length()-1); 
         }
         
         if(open > close){
             sb.append(')');
-            generateParenthesis(n, open, close+1, sb); 
-            sb.deleteCharAt(sb.length()-1); 
+            generateParenthesis(n, open, close + 1, sb);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
