772f670c5d36743c1c72874b40415e7d57588b26
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-10 01:33:57 -0600

    Time: 85 ms (88.50%), Space: 58.7 MB (31.81%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,17 +1,46 @@
 class Solution {
-    public int carFleet(int target, int[] pos, int[] speed) {
-        int N = pos.length, res = 0;
-        double[][] cars = new double[N][2];
-        for (int i = 0; i < N; ++i)
-            cars[i] = new double[] {pos[i], (double)(target - pos[i]) / speed[i]};
-        Arrays.sort(cars, (a, b) -> Double.compare(a[0], b[0]));
-        double cur = 0;
-        for (int i = N - 1; i >= 0 ; --i) {
-            if (cars[i][1] > cur) {
-                cur = cars[i][1];
-                res++;
-            }
+    public int carFleet(int target, int[] position, int[] speed) {
+        Map<Integer,Integer> map = new HashMap(); 
+        
+        for(int i = 0; i < position.length; i++){
+            map.put(position[i], speed[i]); 
         }
-        return res;
+        
+        int[] sortedPositions = new int[position.length];
+        int i = 0; 
+        
+        for(int n: map.keySet()){
+            sortedPositions[i++] = n; 
+        }
+        
+        Arrays.sort(sortedPositions); 
+        
+        Deque<Integer> stack = new ArrayDeque(); 
+        
+        for(int j = 0; j < sortedPositions.length; j++){
+            while(!stack.isEmpty()){
+                int p1 = stack.peek(); 
+                int s1 = map.get(p1); 
+                int p2 = sortedPositions[j];
+                int s2 = map.get(p2); 
+                
+                if(s1 - s2 != 0){
+                    double intersectionTime = (p2 - p1) / ((double)s1 - s2);
+                    double intersectionPosition = p1 + s1 * intersectionTime; 
+                    
+                    if(intersectionTime > 0 && 
+                       target >= intersectionPosition && intersectionPosition >= p2)
+                        stack.pop(); 
+                    else
+                        break; 
+                } else {
+                    break; 
+                }
+            } 
+            
+            stack.push(sortedPositions[j]); 
+        }
+        
+        return stack.size(); 
     }
 }
