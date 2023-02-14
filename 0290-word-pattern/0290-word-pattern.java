73a9ee6619ab51d2ae5e0f0976ebf0c2be9cc60c
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-14 13:34:19 -0600

    Time: 1 ms (90.61%), Space: 39.9 MB (97.18%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -3,34 +3,32 @@ class Solution {
         if(pattern == null || s == null)
             return false; 
         
-        int i = 0; 
+        Map<Character, String> map = new HashMap(); 
+        Map<String, Character> reverseMap = new HashMap(); 
         int j = 0; 
-        Map<Character, String> mapFoward = new HashMap(); 
-        Map<String, Character> mapBackward = new HashMap(); 
-        
-        while(i < pattern.length() && j < s.length()){
-            int begin = j; 
+        int i = 0; 
+        for(i = 0; i < pattern.length() && j < s.length(); i++){
+            char c = pattern.charAt(i);
+            StringBuilder sb = new StringBuilder(); 
             
-            while(j < s.length() && s.charAt(j) != ' ')
-                j++;
+            while(j < s.length() && s.charAt(j) != ' '){
+                 sb.append(s.charAt(j++)); 
+            }
+                    
+            String word = sb.toString(); 
             
-            char key = pattern.charAt(i); 
-            String value = s.substring(begin, j); 
-            
-            if(mapFoward.containsKey(key) && !mapFoward.get(key).equals(value))
-                return false; 
-            
-            if(mapBackward.containsKey(value) && 
-               mapBackward.get(value) != key)
-                return false; 
-            
-            mapFoward.put(key, value);
-            mapBackward.put(value, key);
-            
-            i++; 
             j++; 
+            
+            if(map.containsKey(c) && !map.get(c).equals(word))
+                return false;
+            
+            if(reverseMap.containsKey(word) && reverseMap.get(word) != c)
+                return false;
+            
+            map.put(c, word); 
+            reverseMap.put(word, c);
         }
         
-        return i == pattern.length() && j >= s.length(); 
+        return i == pattern.length() && j > s.length();  
     }
 }
