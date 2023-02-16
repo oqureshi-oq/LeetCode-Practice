28bb73e984b6f7d1917ab6b6b9bdb874258d1e53
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-15 21:07:42 -0600

    Time: 50 ms (76.16%), Space: 79.7 MB (87.93%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.java
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.java
@@ -1,17 +1,12 @@
 class Solution {
     public long interchangeableRectangles(int[][] rectangles) {
         Map<Double, Integer> map = new HashMap(); 
+        long count = 0; 
         
         for(int i = 0; i < rectangles.length; i++){
             double ratio = (double) rectangles[i][0] / rectangles[i][1]; 
+            count += map.getOrDefault(ratio, 0); 
             map.put(ratio, map.getOrDefault(ratio, 0) + 1); 
-        }
-        
-        long count = 0; 
-        
-        for(double n: map.keySet()){
-            double total = map.get(n); 
-            count += (total * (total-1))/2; 
         }
         
         return count; 
