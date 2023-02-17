fd1d6b7e0ad8d687a8517095e22fd0823706033b
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-17 11:55:20 -0600

    Time: 2 ms (31.06%), Space: 49.6 MB (81.55%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -3,18 +3,19 @@ class Solution {
         if(nums == null)
             return 0;
         
-        int min = nums.length+1; 
-        int total = 0; 
+        int min = nums.length+1;
+        int sum = 0; 
         
         for(int left = 0, right = 0; right < nums.length; right++){
-            total += nums[right];
+            sum += nums[right];
             
-            while(total >= target){
-                min = Math.min(min, right - left + 1);
-                total -= nums[left++]; 
-            }
+            while(sum - nums[left] >= target)
+                sum -= nums[left++];
+            
+            if(sum >= target)
+                min = Math.min(min, right - left + 1); 
         }
         
-        return min != nums.length+1 ? min: 0; 
+        return min == nums.length+1 ? 0: min; 
     }
 }
