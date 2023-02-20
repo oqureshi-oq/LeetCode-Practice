e8c96f1d5891e346fb28729dce1820e1429dc5ea
Omar Qureshi <oqureshi.oq@gmail.com>
2023-02-20 00:06:50 -0600

    Time: 0 ms (100.00%), Space: 42.6 MB (91.69%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -4,15 +4,15 @@ class Solution {
             return -1;
         
         int left = 0;
-        int right = nums.length-1; 
-        
+        int right = nums.length - 1;
+         
         while(left <= right){
             int mid = left + (right - left)/2; 
             
             if(nums[mid] == target)
                 return mid;
             else if(nums[mid] < target)
-                left = mid+1;
+                left = mid+1; 
             else
                 right = mid-1; 
         }
