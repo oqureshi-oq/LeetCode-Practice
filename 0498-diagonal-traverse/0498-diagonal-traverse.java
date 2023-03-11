878e2895e806a27d58b7e22efba028581ad0ba8c
Omar Qureshi <oqureshi.oq@gmail.com>
2023-03-10 19:23:28 -0600

    Time: 2 ms (93.06%), Space: 44.3 MB (51.57%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -6,34 +6,38 @@ class Solution {
         int m = mat.length; 
         int n = mat[0].length; 
         int[] ans = new int[m*n]; 
-        boolean goingUp = true; 
         int row = 0;
-        int col = 0; 
+        int col = 0;
+        boolean goingUp = true; 
+        int i = 0; 
         
-        for(int i = 0; i < m*n; i++){
-            ans[i] = mat[row][col]; 
+        
+        while(i < m*n){
+            ans[i++] = mat[row][col];
             
             if(goingUp){
                 if(row-1 >= 0 && col+1 < n){
                     row--;
                     col++;
-                } else if(row-1 < 0 && col+1 < n){
-                    col++; 
+                } else { 
                     goingUp = false; 
-                } else{
-                    row++; 
-                    goingUp = false; 
+                    
+                    if(col+1 < n)
+                        col++;
+                    else
+                        row++; 
                 }
-            } else {
+            } else{
                 if(row+1 < m && col-1 >= 0){
-                    row++; 
+                    row++;
                     col--; 
-                } else if(col-1 < 0 && row+1 < m){
-                    row++; 
-                    goingUp = true; 
                 } else {
-                    col++;
                     goingUp = true; 
+                    
+                    if(row+1 < m)
+                        row++;
+                    else
+                        col++; 
                 }
             }
         }
