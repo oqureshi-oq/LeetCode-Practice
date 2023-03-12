48e25d913106608086af6ae250ab96d0fd2c67dc
Omar Qureshi <oqureshi.oq@gmail.com>
2023-03-12 16:20:11 -0500

    Time: 1 ms (79.53%), Space: 40.1 MB (46.59%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -3,23 +3,22 @@ class Solution {
         if(rowIndex < 0)
             return new ArrayList(); 
         
-        int[] ans = new int[rowIndex+1];  
+        Integer[] row = new Integer[rowIndex+1]; 
         
         for(int i = 0; i <= rowIndex; i++){
-            for(int j = i; j >= 0; j--){
-                if(j == i || j == 0)
-                    ans[j] = 1; 
-                else
-                    ans[j] += ans[j-1]; 
+            Integer prev = 1; 
+            
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i) {
+                    row[j] = 1;
+                } else {
+                    int temp = row[j];
+                    row[j] += prev;
+                    prev = temp; 
+                }
             }
         }
         
-        List<Integer> list = new ArrayList(); 
-        
-        for(int n: ans){
-            list.add(n); 
-        }
-        
-        return list; 
+        return Arrays.asList(row); 
     }
 }
