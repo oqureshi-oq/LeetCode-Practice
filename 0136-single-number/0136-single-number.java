b1179abc1238497391c00daa41a416bb77d4661e
Omar Qureshi <oqureshi.oq@gmail.com>
2023-03-13 11:19:32 -0500

    Time: 1 ms (100.00%), Space: 42.6 MB (38.22%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> seen = new HashSet(); 
-        
-        int expected = 0;
-        int actual = 0; 
+        int x = 0;
         
         for(int n: nums){
-            if(!seen.add(n))
-                expected += 2 * n; 
-            actual += n; 
+            x ^= n;
         }
         
-        return actual - expected; 
+        return x; 
     }
 }
