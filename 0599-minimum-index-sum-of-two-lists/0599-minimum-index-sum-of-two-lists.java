cd88e66df2490c066dfbd38d6728f0528bf47629
Omar Qureshi <oqureshi.oq@gmail.com>
2023-03-14 17:50:50 -0500

    Time: 10 ms (58.58%), Space: 42.8 MB (83.83%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -1,40 +1,36 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        if(list1 == null || list2 == null)
-            return new String[0]; 
-        
         Map<String, Integer> map = new HashMap(); 
-        List<String> ans = new ArrayList();  
-        int minIndexSum = list1.length + list2.length; 
         
         for(int i = 0; i < list1.length; i++){
-            map.put(list1[i], i); 
+            map.put(list1[i], i);
         }
+        
+        Map<Integer, List<String>> map2 = new HashMap(); 
+        int minSum = list1.length + list2.length; 
         
         for(int i = 0; i < list2.length; i++){
-            if(map.containsKey(list2[i])){
-                int indexSum = i + map.get(list2[i]); 
+            if(!map.containsKey(list2[i]))
+                continue; 
+            
+            int sum = map.get(list2[i]) + i; 
+            
+            if(minSum >= sum){
+                if(!map2.containsKey(sum))
+                    map2.put(sum, new ArrayList());
                 
-                if(indexSum < minIndexSum){
-                    ans.clear();
-                    minIndexSum = i + map.get(list2[i]); 
-                }
+                map2.get(sum).add(list2[i]); 
                 
-                if(indexSum <= minIndexSum){
-                    ans.add(list2[i]); 
-                }
-            }
+                minSum = sum; 
+            } 
         }
         
-        if(minIndexSum == list1.length + list2.length)
-            return new String[0];
+        String[] ans = new String[map2.get(minSum).size()];
         
-        String[] answer = new String[ans.size()];
-        
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = ans.get(i); 
+        for(int i = 0; i < map2.get(minSum).size(); i++){
+            ans[i] = map2.get(minSum).get(i); 
         }
         
-        return answer; 
+        return ans; 
     }
 }
