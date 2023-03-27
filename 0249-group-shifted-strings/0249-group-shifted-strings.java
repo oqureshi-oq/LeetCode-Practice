92a331b2b99f0d26391b9e6659cf747b0a121243
Omar Qureshi <oqureshi.oq@gmail.com>
2023-03-27 15:35:14 -0500

    Time: 2 ms (74.14%), Space: 43.2 MB (13.36%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -5,33 +5,23 @@ class Solution {
         
         Map<String, List<String>> map = new HashMap(); 
         
-        for(String str: strings){    
-            if(str == null || str.length() == 0)
-                continue; 
-            
-            char[] strArr = str.toCharArray(); 
-                       
+        for(String str: strings){
             int diff = str.charAt(0) - 'a';
             
+            StringBuilder sb = new StringBuilder(); 
+            
             for(int i = 0; i < str.length(); i++){
-                int c = (str.charAt(i) - diff) % 26;  
-                strArr[i] = (char) c;  
+                sb.append((str.charAt(i) - diff) % 26);
             }
             
-            String key = String.valueOf(strArr); 
+            String key = sb.toString(); 
             
             if(!map.containsKey(key))
-                map.put(key, new ArrayList()); 
+                map.put(key, new ArrayList());
             
             map.get(key).add(str); 
         }
         
-        //return new ArrayList(map.values()); 
-        List<List<String>> groups = new ArrayList<>();
-        for (List<String> group : map.values()) {
-            groups.add(group);
-        }
-        
-        return groups;
+        return new ArrayList(map.values()); 
     }
 }
