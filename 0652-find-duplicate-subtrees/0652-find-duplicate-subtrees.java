060916770afdd0f2e5dace9329ae2935fdfdf8a9
Omar Qureshi <oqureshi.oq@gmail.com>
2023-04-01 15:03:23 -0500

    Time: 23 ms (15.24%), Space: 51.3 MB (19.30%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
         List<TreeNode> res = new LinkedList<>();
-        traverse(root, new HashMap<>(), new HashMap<>(), res);
+        traverse(root, new HashMap<>(), res);
         return res;
     }
 
-    public int traverse(TreeNode node, Map<String, Integer> tripletToID,
-            Map<Integer, Integer> cnt, List<TreeNode> res) {
+    public String traverse(TreeNode node, Map<String, Integer> cnt,
+            List<TreeNode> res) {
         if (node == null) {
-            return 0;
+            return "";
         }
-        String triplet = traverse(node.left, tripletToID, cnt, res) + "," + node.val +
-                "," + traverse(node.right, tripletToID, cnt, res);
-        if (!tripletToID.containsKey(triplet)) {
-            tripletToID.put(triplet, tripletToID.size() + 1);
-        }
-        int id = tripletToID.get(triplet);
-        cnt.put(id, cnt.getOrDefault(id, 0) + 1);
-        if (cnt.get(id) == 2) {
+        String representation = "(" + traverse(node.left, cnt, res) + ")" +
+                node.val + "(" + traverse(node.right, cnt, res) +
+                ")";
+        cnt.put(representation, cnt.getOrDefault(representation, 0) + 1);
+        if (cnt.get(representation) == 2) {
             res.add(node);
         }
-        return id;
+        return representation;
     }
 }
