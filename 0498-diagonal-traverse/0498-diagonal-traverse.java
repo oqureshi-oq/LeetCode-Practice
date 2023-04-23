1c9b890622ae13a13d324dac842182b6dd63fc80
Omar Qureshi <oqureshi.oq@gmail.com>
2023-04-23 17:10:03 -0500

    Time: 2 ms (97.80%), Space: 44.6 MB (21.16%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,43 +1,39 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         if(mat == null)
-            return null; 
+            return null;
         
         int m = mat.length; 
-        int n = mat[0].length; 
-        int[] ans = new int[m*n]; 
+        int n = mat[0].length;
+        int[] ans = new int[m*n];
+        boolean goingUp = true; 
         int row = 0;
         int col = 0;
-        boolean goingUp = true; 
-        int i = 0; 
         
-        
-        while(i < m*n){
-            ans[i++] = mat[row][col];
+        for(int i = 0; i < m*n; i++){
+            ans[i] = mat[row][col];
             
             if(goingUp){
                 if(row-1 >= 0 && col+1 < n){
                     row--;
                     col++;
-                } else { 
-                    goingUp = false; 
-                    
-                    if(col+1 < n)
-                        col++;
-                    else
-                        row++; 
+                } else if(col+1 < n){
+                    col++;
+                    goingUp = !goingUp;
+                } else{
+                    row++; 
+                    goingUp = !goingUp;
                 }
-            } else{
+            } else {
                 if(row+1 < m && col-1 >= 0){
                     row++;
-                    col--; 
+                    col--;
+                } else if(row+1 < m){
+                    row++;
+                    goingUp = !goingUp;
                 } else {
-                    goingUp = true; 
-                    
-                    if(row+1 < m)
-                        row++;
-                    else
-                        col++; 
+                    col++;
+                    goingUp = !goingUp;
                 }
             }
         }
