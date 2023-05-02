e98c66a15e99cc481ae16de429b62ee3f1b96b79
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-01 22:59:34 -0500

    Time: 1 ms (92.95%), Space: 42.8 MB (61.10%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -9,19 +9,18 @@ class Solution {
                 zeroCount++; 
         }
         
+        int write = arr.length + zeroCount - 1;
         int read = arr.length - 1; 
-        int write = arr.length + zeroCount - 1; 
         
         while(read >= 0){
-            if(write < arr.length){
+            if(write < arr.length)
                 arr[write] = arr[read];
-            }
             
             write--; 
             
             if(arr[read] == 0){
                 if(write < arr.length)
-                    arr[write] = 0;
+                    arr[write] = 0; 
                 write--; 
             }
             
