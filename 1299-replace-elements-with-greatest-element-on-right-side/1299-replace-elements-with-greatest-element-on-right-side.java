4c0a66d5a67e50bb7d3588d05322db98041974a1
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-07 22:45:53 -0500

    Time: 2 ms (81.83%), Space: 45.4 MB (52.47%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -3,9 +3,8 @@ class Solution {
         if(arr == null)
             return null;
         
-        int max = -1; 
-        
-        for(int i = arr.length-1; i >= 0; i--){
+        int max = -1;
+        for(int i = arr.length - 1; i >= 0; i--){
             int temp = arr[i];
             arr[i] = max; 
             max = Math.max(max, temp);
