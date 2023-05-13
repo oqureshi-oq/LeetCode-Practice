f2ea02680d5dad60bf3514e2ba9216949eb3273f
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-12 21:37:21 -0500

    Time: 1 ms (100.00%), Space: 42.2 MB (52.48%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -3,23 +3,20 @@ class Solution {
         if(a == null || b == null)
             return "";
         
-        StringBuilder sb = new StringBuilder();
-        int aP = a.length()-1; 
-        int bP = b.length()-1;
+        int aP = a.length()-1;
+        int bP = b.length()-1; 
         int carry = 0;
+        StringBuilder sb = new StringBuilder(); 
         
-        while( aP >= 0 || bP >= 0 || carry == 1){
+        while(aP >= 0 || bP >= 0 || carry == 1){
             int sum = carry;
-            
             if(aP >= 0)
                 sum += a.charAt(aP--) - '0';
             if(bP >= 0)
                 sum += b.charAt(bP--) - '0';
-            
             sb.append(sum % 2);
-            carry = sum / 2; 
+            carry = sum / 2;
         }
-        
         
         return sb.reverse().toString(); 
     }
