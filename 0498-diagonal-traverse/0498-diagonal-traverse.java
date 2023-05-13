10114f13a403068a507c1855f12896b962304a91
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-12 21:08:52 -0500

    Time: 2 ms (96.46%), Space: 44.1 MB (50.65%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         if(mat == null)
-            return null; 
+            return null;
         
         int m = mat.length; 
-        int n = mat[0].length; 
-        int row = 0;
-        int col = 0; 
-        boolean goingUp = true; 
+        int n = mat[0].length;
         int[] ans = new int[m*n];
+        int row = 0;
+        int col = 0;
+        boolean goingUp = true;
         
         for(int i = 0; i < m*n; i++){
             ans[i] = mat[row][col];
@@ -17,22 +17,22 @@ class Solution {
                 if(row-1 >= 0 && col+1 < n){
                     row--;
                     col++;
-                } else if(col+1 < n){
+                } else if(col + 1 < n){
                     col++;
-                    goingUp = false; 
+                    goingUp = false;
                 } else {
-                    row++; 
+                    row++;
                     goingUp = false; 
                 }
             } else {
                 if(row+1 < m && col-1 >= 0){
                     row++;
-                    col--; 
-                } else if(row + 1 < m){
+                    col--;
+                } else if(row+1 < m){
                     row++;
                     goingUp = true;
                 } else {
-                    col++; 
+                    col++;
                     goingUp = true; 
                 }
             }
