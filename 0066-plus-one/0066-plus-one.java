03bdfa52f53b2731f979c67a3d3490789937fcb1
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-12 20:55:47 -0500

    Time: 0 ms (100.00%), Space: 41.3 MB (34.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        if(digits == null || digits.length <= 0)
-            return digits; 
+        if(digits == null)
+            return null;
         
         for(int i = digits.length - 1; i >= 0; i--){
             if(digits[i] < 9){
-                digits[i]++;
-                return digits;
+                digits[i]++; 
+                return digits; 
             }
             
             digits[i] = 0;
         }
         
-        int[] ans = new int[digits.length+1];
-        ans[0] = 1; 
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
         return ans; 
     }
 }
