443c0f40fda755f354c8c6e01ae5d236ef7af319
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-13 22:42:35 -0500

    Time: 14 ms (20.38%), Space: 43.4 MB (9.34%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int x = 0;
+        if(nums == null)
+            return Integer.MIN_VALUE; 
+        
+        Map<Integer, Integer> map = new HashMap(); 
         
         for(int n: nums){
-            x ^= n;
+            map.put(n, map.getOrDefault(n, 0)  +1);
         }
         
-        return x; 
+        for(int n: nums){
+            if(map.get(n) == 1)
+                return n; 
+        }
+        
+        return Integer.MAX_VALUE;
     }
 }
