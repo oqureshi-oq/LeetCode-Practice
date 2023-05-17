9eb381419c424019b15453ea89c6f0aa8eda7e4d
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-17 10:27:02 -0500

    Time: 8 ms (99.22%), Space: 55.8 MB (81.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,7 +3,7 @@ class Solution {
         if(nums == null)
             return false; 
         
-        Set<Integer> seen = new HashSet(); 
+        Set<Integer> seen = new HashSet();
         
         for(int n: nums){
             if(!seen.add(n))
