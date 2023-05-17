c2c36bf62ca97725d9b44c17e5a0fad66cad706f
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-16 22:49:04 -0500

    Time: 9 ms (68.70%), Space: 42.7 MB (39.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,23 +3,21 @@ class Solution {
         if(s == null || t == null || s.length() != t.length())
             return false; 
         
-        int[] sMapT = new int[256];
-        int[] tMapS = new int[256]; 
-        Arrays.fill(sMapT, -1);
-        Arrays.fill(tMapS, -1); 
+        int[] sToT = new int[256];
+        int[] tToS = new int[256];
+        Arrays.fill(sToT, -1);
+        Arrays.fill(tToS, -1);
         
         for(int i = 0; i < s.length(); i++){
-            if(sMapT[s.charAt(i)] != -1 && 
-               sMapT[s.charAt(i)] != t.charAt(i))
+            if(sToT[s.charAt(i)] == -1)
+                sToT[s.charAt(i)] = t.charAt(i);
+            
+            if(tToS[t.charAt(i)] == -1)
+                tToS[t.charAt(i)] = s.charAt(i);
+            
+            if(sToT[s.charAt(i)] != t.charAt(i) || 
+              tToS[t.charAt(i)] != s.charAt(i))
                 return false; 
-            
-            if(tMapS[t.charAt(i)] != -1 && 
-               tMapS[t.charAt(i)] != s.charAt(i))
-                return false; 
-            
-            
-            sMapT[s.charAt(i)] = t.charAt(i);
-            tMapS[t.charAt(i)] = s.charAt(i); 
         }
         
         return true; 
