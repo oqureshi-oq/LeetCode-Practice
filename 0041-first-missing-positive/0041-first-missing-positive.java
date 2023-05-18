2e8e4f51920790691f5a7be309f5ff3078348436
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-17 20:58:53 -0500

    Time: 1 ms (100.00%), Space: 51.2 MB (27.62%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,12 +1,12 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         if(nums == null || nums.length == 0)
-            return 1;
-                
+            return 1; 
+        
         for(int i = 0; i < nums.length; i++){
-            if(1 <= nums[i] && nums[i] <= nums.length && i+1 != nums[i]){
+            if(1 <= nums[i] && nums[i] < nums.length && i+1 != nums[i]){
                 int temp1 = nums[i];
-                int temp2 = nums[nums[i]-1];  
+                int temp2 = nums[nums[i]-1];
                 swap(nums, i, nums[i]-1);
                 if(temp1 != temp2)
                     i--; 
@@ -15,20 +15,15 @@ class Solution {
         
         for(int i = 0; i < nums.length; i++){
             if(i+1 != nums[i])
-                return i+1; 
+                return i+1;
         }
         
         return nums.length+1; 
     }
     
-    public void swap(int[] nums, int i, int j){
+    private void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp; 
     }
 }
-
-// [3, 4, -1, 1]
-// [-1, 4, 3, 1]
-// [-1, 1, 3 ,4]
-// [1, -1, 3, 4]
