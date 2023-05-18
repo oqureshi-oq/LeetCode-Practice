5fdee89ac5040ba5e448fe6c2ad13592221bc96a
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-17 19:02:44 -0500

    Time: 559 ms (5.02%), Space: 51.1 MB (33.30%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,21 +1,36 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        if(nums == null)
-            return -1;
-        
-        Set<Integer> seen = new HashSet(); 
-        int max = 0; 
-        
+        if(nums == null || nums.length == 0)
+            return 1;
+                
+        for(int i = 0; i < nums.length; i++){
+            if(1 <= nums[i] && nums[i] <= nums.length && i+1 != nums.length+1 && i+1 != nums[i]){
+                int temp1 = nums[i];
+                int temp2 = nums[nums[i]-1];  
+                swap(nums, i, nums[i]-1);
+                if(temp1 != temp2)
+                    i--; 
+            }
+        }
         for(int n: nums){
-            seen.add(n);
-            max = Math.max(max, n); 
+            System.out.println(n);
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(i+1 != nums[i])
+                return i+1; 
         }
         
-        for(int i = 1; i < max; i++){
-            if(!seen.contains(i))
-                return i; 
-        }
-        
-        return max+1; 
+        return nums.length+1; 
+    }
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp; 
     }
 }
+
+// [3, 4, -1, 1]
+// [-1, 4, 3, 1]
+// [-1, 1, 3 ,4]
+// [1, -1, 3, 4]
