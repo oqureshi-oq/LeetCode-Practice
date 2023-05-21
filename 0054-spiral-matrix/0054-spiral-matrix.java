3b4fdcf00d07a8c932a4c8f12ee5a97b41ad4a88
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-21 16:07:42 -0500

    Time: 0 ms (100.00%), Space: 40.4 MB (89.75%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,43 +3,43 @@ class Solution {
         if(matrix == null)
             return new ArrayList(); 
         
-        int m = matrix.length;
+        int m = matrix.length; 
         int n = matrix[0].length;
         int topRow = 0;
         int bottomRow = m-1;
         int leftCol = 0;
         int rightCol = n-1;
-        List<Integer> list = new ArrayList(); 
         int i = 0; 
+        List<Integer> list = new ArrayList(); 
         
         while(i < m*n){
             for(int col = leftCol; i < m*n && col <= rightCol; col++){
                 list.add(matrix[topRow][col]);
-                i++; 
+                i++;
             }
             
             topRow++;
             
             for(int row = topRow; i < m*n && row <= bottomRow; row++){
                 list.add(matrix[row][rightCol]);
-                i++;
+                i++; 
             }
             
-            rightCol--;
+            rightCol--; 
             
             for(int col = rightCol; i < m*n && leftCol <= col; col--){
                 list.add(matrix[bottomRow][col]);
-                i++;
+                i++; 
             }
             
             bottomRow--;
             
             for(int row = bottomRow; i < m*n && topRow <= row; row--){
                 list.add(matrix[row][leftCol]);
-                i++;
+                i++; 
             }
             
-            leftCol++; 
+            leftCol++;
         }
         
         return list; 
