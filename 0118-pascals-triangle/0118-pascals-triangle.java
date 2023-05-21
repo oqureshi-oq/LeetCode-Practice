d2a26532bd9a75cc3623603de25e06a552e7e2e9
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-21 16:20:43 -0500

    Time: 1 ms (93.58%), Space: 40.9 MB (98.14%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -12,7 +12,7 @@ class Solution {
                 if(j == 0 || j == i)
                     row.add(1);
                 else
-                    row.add(list.get(i-1).get(j-1) + list.get(i-1).get(j));
+                    row.add( list.get(i-1).get(j-1) + list.get(i-1).get(j) );
             }
             
             list.add(row);
