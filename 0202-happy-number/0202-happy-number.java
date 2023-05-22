962bfc7a473506541701bf6d9cd3d377c9dec590
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-22 17:01:55 -0500

    Time: 1 ms (89.23%), Space: 39.7 MB (65.89%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,8 +3,9 @@ class Solution {
         if(n <= 0)
             return false; 
         
-        int slow = n;
+        int slow = next(n);
         int fast = next(next(n));
+        
         while(fast != slow && fast != 1){
             slow = next(slow);
             fast = next(next(fast));
@@ -17,11 +18,10 @@ class Solution {
         int sum = 0;
         
         while(n != 0){
-            int digit = n % 10; 
-            sum += digit * digit; 
-            n /= 10; 
+            sum += (n % 10) * (n % 10);
+            n /= 10;
         }
         
-        return sum;
+        return sum; 
     }
 }
