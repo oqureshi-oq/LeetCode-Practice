e8c1369e19222f46c8db973be495709ff19ea8c9
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-22 23:45:12 -0500

    Time: 11 ms (94.74%), Space: 47.9 MB (5.03%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         if(nums == null)
-            return new int[0];
+            return null;
         
-        Map<Integer, Integer> counts = new HashMap(); 
+        Map<Integer, Integer> map = new HashMap(); 
         
         for(int n: nums){
-            counts.put(n, counts.getOrDefault(n, 0) + 1); 
+            map.put(n, map.getOrDefault(n, 0)+1);
         }
         
-        List<Integer>[] buckets = new List[nums.length+1]; 
+        List<Integer>[] buckets = new List[nums.length+1];
         
-        for(int n: counts.keySet()){
-            int count = counts.get(n);
+        for(int key: map.keySet()){
+            int count = map.get(key);
             
             if(buckets[count] == null)
                 buckets[count] = new ArrayList(); 
             
-            buckets[count].add(n); 
+            buckets[count].add(key);
         }
         
         int[] ans = new int[k]; 
         
         for(int i = buckets.length-1; k > 0 && i >= 0; i--){
             if(buckets[i] == null)
-                continue;
+                continue; 
             
             for(int j = 0; k > 0 && j < buckets[i].size(); j++){
-                ans[k-1] = buckets[i].get(j); 
+                ans[k-1] = buckets[i].get(j);
                 k--; 
             }
         }
