1f1e598274a6b61fbbc6f3b1c9cc467bec4f942b
Omar Qureshi <oqureshi.oq@gmail.com>
2023-05-24 23:15:24 -0500

    Time: 11 ms (92.24%), Space: 44.6 MB (87.37%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -1,25 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    private Map<String, Integer> subTreeToId;
+    private Map<Integer, Integer> subtrees; 
+    private List<TreeNode> duplicates; 
+    
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        List<TreeNode> res = new LinkedList<>();
-        traverse(root, new HashMap<>(), new HashMap<>(), res);
-        return res;
+        subtrees = new HashMap(); 
+        subTreeToId = new HashMap(); 
+        duplicates = new ArrayList();
+        helper(root); 
+        return duplicates; 
     }
-
-    public int traverse(TreeNode node, Map<String, Integer> tripletToID,
-            Map<Integer, Integer> cnt, List<TreeNode> res) {
-        if (node == null) {
+    
+    public int helper(TreeNode node){
+        if(node == null)
             return 0;
-        }
-        String triplet = traverse(node.left, tripletToID, cnt, res) + "," + node.val +
-                "," + traverse(node.right, tripletToID, cnt, res);
-        if (!tripletToID.containsKey(triplet)) {
-            tripletToID.put(triplet, tripletToID.size() + 1);
-        }
-        int id = tripletToID.get(triplet);
-        cnt.put(id, cnt.getOrDefault(id, 0) + 1);
-        if (cnt.get(id) == 2) {
-            res.add(node);
-        }
-        return id;
+        
+        int left = helper(node.left);
+        int right = helper(node.right);
+        String subTree = left + ", " + node.val + ", " + right;
+        
+        if(!subTreeToId.containsKey(subTree))
+            subTreeToId.put(subTree, subTreeToId.size() + 1);
+        
+        int id = subTreeToId.get(subTree); 
+        
+        subtrees.put(id, subtrees.getOrDefault(id, 0) + 1);
+        
+        if(subtrees.get(id) == 2)
+            duplicates.add(node);
+        
+        return id; 
     }
 }
