d988ae632918867e7f34794b9d976768320542e5
Omar Qureshi <oqureshi.oq@gmail.com>
2023-06-07 11:28:26 -0500

    Time: 3 ms (41.99%), Space: 44.4 MB (67.83%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -4,15 +4,15 @@ class Solution {
             return 0;
         
         int count = 0;
-        int maxCount = 0;
+        int max = 0;
         
         for(int n: nums){
             if(n == 1)
-                maxCount = Math.max(++count, maxCount);
+                max = Math.max(++count, max);
             else
                 count = 0;
         }
         
-        return maxCount; 
+        return max; 
     }
 }
