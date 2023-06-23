0b1d63e509cb05063354de233d5c49e3a84cc4d8
Omar Qureshi <oqureshi.oq@gmail.com>
2023-06-22 20:03:00 -0500

    Time: 0 ms (100.00%), Space: 40.3 MB (77.96%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -8,17 +8,17 @@ class Solution {
             expected[h]++;
         }
         
-        int count = 0;
-        int eH = 1;
+        int count = 0; 
+        int h = 1;
         
         for(int aH: heights){
-            while(expected[eH] == 0)
-                eH++;
+            while(expected[h] == 0)
+                h++;
             
-            if(eH != aH)
+            if(h != aH)
                 count++;
             
-            expected[eH]--; 
+            expected[h]--; 
         }
         
         return count; 
