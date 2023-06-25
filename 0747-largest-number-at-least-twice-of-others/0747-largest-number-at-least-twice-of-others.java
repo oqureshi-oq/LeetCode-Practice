9cbf34dcb0b1d1193b7a27a5085c38dbd555e941
Omar Qureshi <oqureshi.oq@gmail.com>
2023-06-25 18:05:51 -0500

    Time: 0 ms (100.00%), Space: 40.6 MB (61.52%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -3,18 +3,20 @@ class Solution {
         if(nums == null || nums.length == 0)
             return -1;
         
-        int firstIndex = -1;
-        int secondIndex = -1; 
+        Integer max = nums[0];
+        int index = 0; 
+        Integer secondMax = null; 
         
-        for(int i = 0; i < nums.length; i++){
-            if(firstIndex == -1 || nums[i] >= nums[firstIndex]){
-                secondIndex = firstIndex;
-                firstIndex = i; 
-            } else if(secondIndex == -1 || nums[i] >= nums[secondIndex]){
-                secondIndex = i;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > max){
+                secondMax = max; 
+                max = nums[i];
+                index = i; 
+            } else if (secondMax == null || nums[i] > secondMax){
+                secondMax = nums[i];
             }
         }
         
-        return nums[firstIndex] >= 2 * nums[secondIndex] ? firstIndex: -1; 
+        return secondMax != null && max >= 2*secondMax ? index: -1; 
     }
 }
