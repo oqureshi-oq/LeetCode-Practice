6880353fc417a27904508b0032f3eeb932e2db7c
Omar Qureshi <oqureshi.oq@gmail.com>
2023-08-14 12:21:28 -0500

    Time: 1 ms (100.00%), Space: 44.8 MB (55.28%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
         if(arr == null || arr.length < 3)
-            return false; 
+            return false;
         
         int left = 0;
-        while(left+1 < arr.length - 1 && arr[left] < arr[left+1])
+        while(left+1 < arr.length-1 && arr[left] < arr[left+1])
             left++;
         
-        int right = arr.length-1;
+        int right = arr.length - 1;
         while(right-1 > 0 && arr[right-1] > arr[right])
             right--;
         
