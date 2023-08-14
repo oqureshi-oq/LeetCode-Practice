a792e295688d559e77358be25fcfcaa9690eaee6
Omar Qureshi <oqureshi.oq@gmail.com>
2023-08-14 10:43:53 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (39.70%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       if(nums1 == null || nums1.length != m+n || nums2 == null || nums2.length != n)
-           return;
+        if(nums1 == null || nums1.length != m+n || nums2 == null || nums2.length != n)
+            return;
         
         int read1 = m-1;
         int read2 = n-1;
@@ -13,7 +13,7 @@ class Solution {
             else
                 nums1[write] = nums2[read2--];
             
-            write--;
+            write--; 
         }
         
         while(read2 >= 0)
