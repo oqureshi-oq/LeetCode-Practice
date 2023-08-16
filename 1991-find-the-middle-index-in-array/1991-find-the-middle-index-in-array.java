6cdde5d9560a8ff712a5b73b688bf357b29edf0d
Omar Qureshi <oqureshi.oq@gmail.com>
2023-08-15 21:33:09 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (33.91%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -9,11 +9,10 @@ class Solution {
         }
         
         int leftSum = 0;
-        
         for(int i = 0; i < nums.length; i++){
             rightSum -= nums[i];
             if(leftSum == rightSum)
-                return i; 
+                return i;
             leftSum += nums[i];
         }
         
