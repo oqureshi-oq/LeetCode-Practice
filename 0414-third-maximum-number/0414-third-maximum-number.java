ded07aef12fd12589fb680953a84ca040fab7112
Omar Qureshi <oqureshi.oq@gmail.com>
2023-08-15 19:49:03 -0500

    Time: 5 ms (35.65%), Space: 43.5 MB (61.42%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,41 +1,27 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         Set<Integer> taken = new HashSet<Integer>();
         
-        for (int index = 0; index < nums.length; ++index) {
-            // If current number was already taken, skip it.
-            if (taken.contains(nums[index])) {
-                continue;
-            }
-            
-            // If min heap already has three numbers in it.
-            // Pop the smallest if current number is bigger than it.
-            if (minHeap.size() == 3) {
-                if (minHeap.peek() < nums[index]) {
+        for(int n: nums){
+            if(minHeap.size() == 3){
+                if(minHeap.peek() < n && !taken.contains(n)){
                     taken.remove(minHeap.poll());
-                    
-                    minHeap.add(nums[index]);
-                    taken.add(nums[index]);
+                    minHeap.add(n);
+                    taken.add(n);
                 }
-            } 
-            // If min heap does not have three number we can push it.
-            else {
-                minHeap.add(nums[index]);
-                taken.add(nums[index]);
+            } else if (!taken.contains(n)){
+                minHeap.add(n);
+                taken.add(n);
             }
         }
         
-        // 'nums' has only one distinct element it will be the maximum.
-        if (minHeap.size() == 1) {
-            return minHeap.peek();
-        }
-        // 'nums' has two distinct elements.
-        else if (minHeap.size() == 2) {
-            int firstNum = minHeap.poll();
-            return Math.max(firstNum, minHeap.peek());
-        }
+        if(minHeap.size() == 3)
+            return minHeap.poll();
         
-        return minHeap.peek();
+        int ans = minHeap.poll(); 
+        while(!minHeap.isEmpty())
+            ans = minHeap.poll(); 
+        return ans; 
     }
 }
