0ee82e097d6a69c231d7fb956973e0745d3c3aba
Omar Qureshi <oqureshi.oq@gmail.com>
2023-08-15 19:58:12 -0500

    Time: 9 ms (14.79%), Space: 43.3 MB (80.22%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,27 +1,42 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        Set<Integer> taken = new HashSet<Integer>();
+        Pair<Integer, Boolean> firstMax = new Pair<Integer, Boolean>(-1, false);
+        Pair<Integer, Boolean> secondMax = new Pair<Integer, Boolean>(-1, false);
+        Pair<Integer, Boolean> thirdMax = new Pair<Integer, Boolean>(-1, false);
         
-        for(int n: nums){
-            if(minHeap.size() == 3){
-                if(minHeap.peek() < n && !taken.contains(n)){
-                    taken.remove(minHeap.poll());
-                    minHeap.add(n);
-                    taken.add(n);
-                }
-            } else if (!taken.contains(n)){
-                minHeap.add(n);
-                taken.add(n);
+        for (int num : nums) {
+            // If current number is already stored, skip it.
+            if ((firstMax.getValue() && firstMax.getKey() == num) || 
+                (secondMax.getValue() && secondMax.getKey() == num) || 
+                (thirdMax.getValue() && thirdMax.getKey() == num)) {
+                continue;
+            }
+            
+            // If we never stored any variable in firstMax
+            // or curr num is bigger than firstMax, then curr num is the biggest number.
+            if (!firstMax.getValue() || firstMax.getKey() < num) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = new Pair<Integer, Boolean>(num, true);
+            }
+            // If we never stored any variable in secondMax
+            // or curr num is bigger than secondMax, then curr num is 2nd biggest number.
+            else if (!secondMax.getValue() || secondMax.getKey() < num) {
+                thirdMax = secondMax;
+                secondMax = new Pair<Integer, Boolean>(num, true);
+            }
+            // If we never stored any variable in thirdMax
+            // or curr num is bigger than thirdMax, then curr num is 3rd biggest number.
+            else if (!thirdMax.getValue() || thirdMax.getKey() < num) {
+                thirdMax = new Pair<Integer, Boolean>(num, true);
             }
         }
         
-        if(minHeap.size() == 3)
-            return minHeap.poll();
+        // If third max was never updated, it means we don't have 3 distinct numbers.
+        if (!thirdMax.getValue()) {
+            return firstMax.getKey();
+        }
         
-        int ans = minHeap.poll(); 
-        while(!minHeap.isEmpty())
-            ans = minHeap.poll(); 
-        return ans; 
+        return thirdMax.getKey();
     }
 }
