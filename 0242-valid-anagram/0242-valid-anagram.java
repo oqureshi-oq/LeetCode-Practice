84dcca063f9c6a353c3c12722587fd84f6be0b0c
Omar Qureshi <oqureshi.oq@gmail.com>
2023-08-16 23:31:41 -0500

    Time: 4 ms (82.71%), Space: 42 MB (81.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,19 @@
 class Solution {
-    final int NUMBER_OF_LETTERS = 26; 
     public boolean isAnagram(String s, String t) {
         if(s == null || t == null || s.length() != t.length())
             return false; 
         
-        int[] letterCounts = new int[NUMBER_OF_LETTERS];
+        int[] letters = new int[26];
+        int length = s.length(); 
         
-        for(int i = 0; i < s.length(); i++){
-            letterCounts[s.charAt(i) - 'a']++;
-            letterCounts[t.charAt(i) - 'a']--; 
+        for(int i = 0; i < length; i++){
+            letters[s.charAt(i) - 'a']++;
+            letters[t.charAt(i)- 'a']--;
         }
         
-        for(int n: letterCounts){
+        for(int n: letters){
             if(n != 0)
-                return false;  
+                return false; 
         }
         
         return true; 
