771e43e90d4154ae38420e01deab357dd534ab95
Omar Qureshi <oqureshi.oq@gmail.com>
2023-09-14 18:06:32 -0500

    Time: 3 ms (77.71%), Space: 43.7 MB (21.35%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,42 +1,28 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Pair<Integer, Boolean> firstMax = new Pair<Integer, Boolean>(-1, false);
-        Pair<Integer, Boolean> secondMax = new Pair<Integer, Boolean>(-1, false);
-        Pair<Integer, Boolean> thirdMax = new Pair<Integer, Boolean>(-1, false);
+        if(nums == null || nums.length == 0)
+            return Integer.MIN_VALUE;
         
-        for (int num : nums) {
-            // If current number is already stored, skip it.
-            if ((firstMax.getValue() && firstMax.getKey() == num) || 
-                (secondMax.getValue() && secondMax.getKey() == num) || 
-                (thirdMax.getValue() && thirdMax.getKey() == num)) {
+        Integer first = null;
+        Integer second = null;
+        Integer third = null; 
+        
+        for(Integer n: nums){
+            if(n.equals(first) || n.equals(second) || n.equals(third))
                 continue;
-            }
             
-            // If we never stored any variable in firstMax
-            // or curr num is bigger than firstMax, then curr num is the biggest number.
-            if (!firstMax.getValue() || firstMax.getKey() < num) {
-                thirdMax = secondMax;
-                secondMax = firstMax;
-                firstMax = new Pair<Integer, Boolean>(num, true);
-            }
-            // If we never stored any variable in secondMax
-            // or curr num is bigger than secondMax, then curr num is 2nd biggest number.
-            else if (!secondMax.getValue() || secondMax.getKey() < num) {
-                thirdMax = secondMax;
-                secondMax = new Pair<Integer, Boolean>(num, true);
-            }
-            // If we never stored any variable in thirdMax
-            // or curr num is bigger than thirdMax, then curr num is 3rd biggest number.
-            else if (!thirdMax.getValue() || thirdMax.getKey() < num) {
-                thirdMax = new Pair<Integer, Boolean>(num, true);
+            if(first == null || n > first){
+                third = second;
+                second = first;
+                first = n;
+            } else if(second == null || n > second){
+                third = second;
+                second = n;
+            } else if(third == null || n > third){
+                third = n;
             }
         }
         
-        // If third max was never updated, it means we don't have 3 distinct numbers.
-        if (!thirdMax.getValue()) {
-            return firstMax.getKey();
-        }
-        
-        return thirdMax.getKey();
+        return third != null ? third: first; 
     }
 }
