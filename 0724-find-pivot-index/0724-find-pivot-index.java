8eb9319b208b9fb971e2adcada3a00529be162a4
Omar Qureshi <oqureshi.oq@gmail.com>
2023-12-15 17:56:03 -0600

    Time: 1 ms (99.08%), Space: 44.6 MB (16.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -12,11 +12,13 @@ class Solution {
         
         for(int i = 0; i < nums.length; i++){
             rightSum -= nums[i];
+            
             if(leftSum == rightSum)
                 return i;
+            
             leftSum += nums[i];
         }
         
-        return -1;
+        return -1; 
     }
 }
