83d749d05aebcc87626db4b0499fe25370559e25
Omar Qureshi <oqureshi.oq@gmail.com>
2024-01-16 19:20:48 -0600

    Time: 0 ms (100.00%), Space: 42 MB (42.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,24 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Make a copy of the first m elements of nums1.
-        int[] nums1Copy = new int[m];
-        for (int i = 0; i < m; i++) {
-            nums1Copy[i] = nums1[i];
+        if(nums1 == null || nums2 == null || nums1.length != m+n || nums2.length != n)
+            return;
+        
+        int read1 = m-1;
+        int read2 = n-1;
+        int write = m+n-1;
+        
+        while(read1 >= 0 && read2 >= 0){
+            if(nums1[read1] > nums2[read2])
+                nums1[write] = nums1[read1--];
+            else
+                nums1[write] = nums2[read2--];
+            
+            write--; 
         }
-
-        // Read pointers for nums1Copy and nums2 respectively.
-        int p1 = 0;
-        int p2 = 0;
-                
-        // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
-        for (int p = 0; p < m + n; p++) {
-            // We also need to ensure that p1 and p2 aren't over the boundaries
-            // of their respective arrays.
-            if (p1 >= m || (p2 < n && nums1Copy[p1] > nums2[p2])) {
-                nums1[p] = nums2[p2++];
-            } else {
-                nums1[p] = nums1Copy[p1++];
-            }
-        }
+        
+        while(read2 >= 0)
+            nums1[write--] = nums2[read2--]; 
     }
 }
