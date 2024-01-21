a03d53b7b58cee0b0b7d119c818c96981f38167d
Omar Qureshi <oqureshi.oq@gmail.com>
2024-01-21 02:36:06 -0600

    Time: 1 ms (100.00%), Space: 42.4 MB (34.24%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,12 +1,12 @@
 class Solution {
     public String addBinary(String a, String b) {
         if(a == null || b == null)
-            return ""; 
+            return null; 
         
-        int carry = 0;
-        StringBuilder sb = new StringBuilder(); 
         int aP = a.length()-1;
         int bP = b.length()-1;
+        StringBuilder str = new StringBuilder();
+        int carry = 0;
         
         while(aP >= 0 || bP >= 0 || carry == 1){
             int sum = carry;
@@ -17,11 +17,11 @@ class Solution {
             if(bP >= 0)
                 sum += b.charAt(bP--) - '0';
             
-            sb.append(sum % 2);
-            
-            carry = sum / 2; 
+            carry = sum / 2;
+            str.append(sum % 2);
         }
         
-        return sb.reverse().toString(); 
+        
+        return str.reverse().toString(); 
     }
 }
