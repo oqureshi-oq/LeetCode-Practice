a8b5a0b4332fde546cbf6bb29b4922c863780369
Omar Qureshi <oqureshi.oq@gmail.com>
2024-01-22 20:35:26 -0600

    Time: 1 ms (81.32%), Space: 41 MB (66.25%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,13 +3,16 @@ class Solution {
         if(strs == null || strs.length == 0)
             return "";
         
+        
         for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            
             for(int j = 1; j < strs.length; j++){
-                if(i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
-                    return strs[0].substring(0, i);
+                if(strs[j].length() == i || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i); 
             }
         }
         
-        return strs[0];
+        return strs[0]; 
     }
 }
