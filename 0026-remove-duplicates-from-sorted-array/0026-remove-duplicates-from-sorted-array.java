9d38b76300916ae4a30dec7f5171afa793f5065a
Omar Qureshi <oqureshi.oq@gmail.com>
2024-02-07 20:57:30 -0600

    Time: 1 ms (81.84%), Space: 44.8 MB (42.16%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -6,7 +6,7 @@ class Solution {
         int k = 1; 
         
         for(int i = 1; i < nums.length; i++){
-            if(nums[i] != nums[k-1])
+            if(nums[k-1] != nums[i])
                 nums[k++] = nums[i];
         }
         
