5d515b01d2c67ea108776c5df4ffdfa50ae5eeb9
Omar Qureshi <oqureshi.oq@gmail.com>
2024-03-27 23:37:14 -0500

    Time: 11 ms (93.39%), Space: 45.2 MB (94.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,29 +6,28 @@ class Solution {
         Map<Integer, Integer> map = new HashMap(); 
         
         for(int n: nums){
-            map.put(n, map.getOrDefault(n, 0)+1);
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
         
-        List<Integer>[] buckets = new List[nums.length+1];
+        List[] buckets = new List[nums.length+1];
         
         for(int key: map.keySet()){
             int count = map.get(key);
             
             if(buckets[count] == null)
-                buckets[count] = new ArrayList(); 
+                buckets[count] = new LinkedList(); 
             
             buckets[count].add(key);
         }
         
-        int[] ans = new int[k]; 
+        int[] ans = new int[k--];
         
-        for(int i = buckets.length-1; k > 0 && i >= 0; i--){
+        for(int i = buckets.length-1; k >= 0 && i >= 0; i--){
             if(buckets[i] == null)
                 continue; 
             
-            for(int j = 0; k > 0 && j < buckets[i].size(); j++){
-                ans[k-1] = buckets[i].get(j);
-                k--; 
+            for(int j = 0; j < buckets[i].size() && k >= 0; j++){
+                ans[k--] = (int) buckets[i].get(j); 
             }
         }
         
