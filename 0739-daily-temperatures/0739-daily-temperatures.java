0d2edb5fab9ba942c855a4890efe16e70f0d9bea
Omar Qureshi <oqureshi.oq@gmail.com>
2024-04-21 00:06:03 -0500

    Time: 24 ms (85.25%), Space: 56.6 MB (89.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         if(temperatures == null)
-            return new int[0];
+            return null;
         
         int[] ans = new int[temperatures.length];
         Deque<Integer> stack = new ArrayDeque(); 
         
         for(int i = 0; i < temperatures.length; i++){
             while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
-                ans[stack.peek()] = i - stack.peek(); 
-                stack.pop();
+                int index = stack.pop(); 
+                ans[index] = i - index; 
             }
             
-            stack.push(i); 
+            stack.push(i);
         }
         
         while(!stack.isEmpty())
