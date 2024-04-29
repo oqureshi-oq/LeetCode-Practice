4b077305fad282a414859ba872b61ded5c58a92e
Omar Qureshi <oqureshi.oq@gmail.com>
2024-04-29 14:47:39 -0500

    Time: 6 ms (18.42%), Space: 55.4 MB (74.74%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int x = k;
+        
+        for(int n: nums){
+            x ^= n; 
+        }
+        
+        int count = 0;
+        System.out.println(x); 
+        while(x != 0){
+            if((x & 1) == 1)
+                count++; 
+            
+            x = x >> 1; 
+        }
+        
+        return count; 
+    }
+}
