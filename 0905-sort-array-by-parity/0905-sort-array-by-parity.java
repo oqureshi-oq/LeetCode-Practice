6aa211eb20911da9fa51b8808f31253a9e6b7a45
Omar Qureshi <oqureshi.oq@gmail.com>
2024-06-04 18:54:27 -0500

    Time: 0 ms (100.00%), Space: 44.7 MB (66.35%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -3,16 +3,16 @@ class Solution {
         if(nums == null)
             return null;
         
-        int evenIndex = 0;
+        int evenPointer = 0;
         
         for(int i = 0; i < nums.length; i++){
             if(nums[i] % 2 == 0){
-                int temp = nums[evenIndex];
-                nums[evenIndex++] = nums[i];
-                nums[i] = temp; 
+                int temp = nums[i];
+                nums[i] = nums[evenPointer];
+                nums[evenPointer++] = temp;
             }
         }
         
-        return nums; 
+        return nums;
     }
 }
