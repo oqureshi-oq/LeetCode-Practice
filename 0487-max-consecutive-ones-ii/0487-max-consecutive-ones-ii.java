1c6b0d1949aff0eab2bd311db0d68dc20feda8a3
Omar Qureshi <oqureshi.oq@gmail.com>
2024-06-16 15:04:17 -0500

    Time: 2 ms (97.59%), Space: 45.2 MB (80.96%) - LeetHub

--- 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.java
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.java
@@ -4,15 +4,15 @@ class Solution {
             return 0;
         
         int k = 1;
-        int left = 0;
         int right = 0;
+        int left = 0;
         
         while(right < nums.length){
             if(nums[right++] == 0)
                 k--;
             
             if(k < 0 && nums[left++] == 0)
-                k++; 
+                k++;
         }
         
         return right - left; 
