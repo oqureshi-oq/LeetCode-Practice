3e547fecbc70ddb780b76908fd7cbb14332e8075
Omar Qureshi <oqureshi.oq@gmail.com>
2024-07-21 22:09:32 -0500

    Time: 2 ms (65.10%), Space: 45 MB (65.34%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,11 +1,7 @@
-/*
- * Time: O(n)
- * Space: O(1)
- */
 class Solution {
     public boolean validMountainArray(int[] arr) {
         if(arr == null || arr.length < 3)
-            return false;
+            return false; 
         
         int left = 0;
         while(left+1 < arr.length-1 && arr[left] < arr[left+1])
@@ -13,7 +9,7 @@ class Solution {
         
         int right = arr.length-1;
         while(0 < right-1 && arr[right-1] > arr[right])
-            right--;
+            right--; 
         
         return left == right; 
     }
