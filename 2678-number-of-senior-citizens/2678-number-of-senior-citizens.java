413e5437d17e0979303adc928e2a1c4d19e1124a
Omar Qureshi <oqureshi.oq@gmail.com>
2024-08-01 13:33:49 -0500

    Time: 1 ms (82.64%), Space: 42.5 MB (45.62%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -1,10 +1,11 @@
+// Time: O(n)
+// Space: O(1) 
 class Solution {
     public int countSeniors(String[] details) {
         int count = 0;
         
         for(String str: details){
-            int age = (str.charAt(11) - '0') * 10 
-                + (str.charAt(12) - '0');
+            int age = (str.charAt(11) - '0') * 10 + (str.charAt(12) - '0');
             
             if(age > 60)
                 count++; 
